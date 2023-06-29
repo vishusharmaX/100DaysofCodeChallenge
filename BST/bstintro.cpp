1db90010649ace3c3f4f1451c0ad44f08c576169
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2023-06-29 22:29:46 +0530

    Update bstintro.cpp

--- BST/bstintro.cpp
+++ BST/bstintro.cpp
@@ -205,6 +205,7 @@ int main(){
     cout<<"Printing preorder"<<endl;
     preorder(root);
     cout<<endl;
+    cout<<endl;
     cout<<"Printing postorder"<<endl;
     postorder(root);
     cout<<endl;
