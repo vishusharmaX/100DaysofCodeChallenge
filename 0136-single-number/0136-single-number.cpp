374623215819d201588391c9a602087822abb5c0
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2023-09-20 22:42:53 +0530

    Time: 19 ms (38.92%), Space: 20.6 MB (8.01%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){{
+            mp[nums[i]]++;
+        }}
+        for(auto i :  mp){
+            if(i.second == 1){
+                ans = i.first;
+                break;
+            }
+            
+        }
+
+        return ans;
+    }
+};
