7ddc0e3b62250ba4ce471743b8e2c4f36c2acb83
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2023-12-24 22:10:52 +0530

    Time: 3 ms (78.34%), Space: 7.4 MB (45.1%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,25 +4,22 @@ public:
         int n = s.length();
 
         int start_with_0 = 0; //010101
-        int start_with_1 = 0; //101010
+        // int start_with_1 = 0; //101010
          
          for(int i = 0 ; i < n ; i++){
-             if(i%2 == 0){
-                 if(s[i] == '0'){
-                     start_with_1++;
-                 }
-                 else
-                    start_with_0++;
-             }
-             else{
+             if(i%2 == 0){ //Even
                  if(s[i] == '1'){
-                     start_with_1++;
+                     start_with_0++;
                  }
-                 else{
+             }
+             else{ //odd
+                 if(s[i] == '0'){
                      start_with_0++;
                  }
              }
          }
+         int start_with_1 = n -start_with_0;
+         
         return min(start_with_0,start_with_1);
     }
 };
