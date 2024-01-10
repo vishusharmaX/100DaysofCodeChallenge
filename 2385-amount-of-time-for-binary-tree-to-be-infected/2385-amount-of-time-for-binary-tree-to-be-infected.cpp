f7620d3c3694136ab1a2804159bf52df860764cc
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-01-10 22:03:51 +0530

    Time: 658 ms (15.64%), Space: 317.7 MB (5.27%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void makeGraph( unordered_map<int,vector<int>>&adj, int parent,TreeNode* curr ){
+        if(curr == NULL)
+            return ;
+
+        if(parent != -1)
+            adj[curr->val].push_back(parent);
+        
+        if(curr->left)
+            adj[curr->val].push_back(curr->left->val);
+         if(curr->right)
+            adj[curr->val].push_back(curr->right->val);
+        
+        makeGraph(adj,curr->val,curr->left);
+        makeGraph(adj,curr->val,curr->right);
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        unordered_map<int,vector<int>> adj;
+
+        makeGraph(adj,-1,root);
+
+        //Apply BFS Travesal
+
+        queue<int> q;
+        unordered_set<int> visited;
+        q.push(start);
+        visited.insert(start);
+
+        int mintues = 0 ;
+
+        while(!q.empty()){
+            int n = q.size();
+            
+            while(n--){
+                int curr = q.front();
+                q.pop();
+
+                for(int &ngbr: adj[curr]){
+                    if(visited.find(ngbr) == visited.end()){
+                         q.push(ngbr);
+                         visited.insert(ngbr);
+                    }
+                }
+            }
+            mintues++;
+        }
+
+        return mintues-1;
+    }
+};
