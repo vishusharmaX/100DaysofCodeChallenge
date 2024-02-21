1bfe7fd5df9c583d0eb3a98a94d83a5b804bd4ce
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-02-22 00:20:22 +0530

    Updated Solution - GfG to GitHub

--- Easy/Key Pair/key-pair.cpp
+++ Easy/Key Pair/key-pair.cpp
@@ -11,24 +11,22 @@ public:
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
-	    sort(arr,arr+n);
 	    
-	    int s = 0;
-	    int e = n-1;
-	    while(s<e){
-	        if(arr[s] + arr[e] == x){
-	            return 1;
-	        }
-	        else if(arr[s] + arr[e] > x){
-	            e--;
-	        }
-	        else{
-	            s++;
-	        }
+	    // code here
+	    int i = 0;
+	    int j = n-1;
+	    sort(arr,arr+n);
+	    while(i <  j){
+	       if(arr[i] + arr[j] ==  x)
+	        return 1;
+	       
+	       else if( arr[i] + arr[j] < x)
+	                i++;
+	       else
+	           j--;
 	    }
 	    
-	    return  0 ;
-	    
+	    return 0;
 	}
 };
 
