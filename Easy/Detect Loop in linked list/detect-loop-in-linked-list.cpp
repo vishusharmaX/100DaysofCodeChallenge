d3e6810ab2d5afdfd7ac6bdf9b81b29d657df8be
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-02-24 14:28:25 +0530

    Updated Solution - GfG to GitHub

--- Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
+++ Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -48,22 +48,19 @@ class Solution
     //Function to check if the linked list has a loop.
     bool detectLoop(Node* head)
     {
-     
         // your code here
-        Node * fast = head;
+        Node * fast  = head;
         Node * slow = head;
         
-        while(fast && fast->next){
+        while(fast != NULL && fast->next != NULL){
             fast = fast->next->next;
-            slow= slow->next;
+            slow = slow->next;
             
-            if(fast == slow){
+            if(fast == slow)
                 return true;
-            }
         }
         
         return false;
-        
     }
 };
 
