db83255878c48f5293d8cf089b038ecc298eb170
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-04 00:49:45 +0530

    Time: 16 ms (89.71%), Space: 27.4 MB (39.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         k = k%n;
         k = nums.size()-k;
-        // if(nums.size()<=1)
-        // return;
+        if(nums.size()<=1)
+        return;
         reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
         reverse(nums.begin(),nums.end());
