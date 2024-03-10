891e8fbf8f2cbbd7d9b86d28f16e4753327edfa9
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-10 11:43:15 +0530

    Time: 24 ms (80.41%), Space: 11.9 MB (24.24%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,8 @@
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        
+        return intersection(nums1,nums2)
+        
