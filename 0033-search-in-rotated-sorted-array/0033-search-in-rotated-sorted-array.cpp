97d897af41ffed3f1b2d3613cd9958bca64ae1d8
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-13 19:24:22 +0530

    Time: 6 ms (17.67%), Space: 16.3 MB (5.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-     int n = nums.size();
-      int left = 0;
-      int right = n-1;
-      int mid= left + (right - left) / 2;
-      while(left <= right){
-        if(nums[mid] == target)
-         return mid;
-        if(nums[mid] >= nums[left]) {
-          if(target >= nums[left] && target <= nums[mid])
-            {
-                right = mid - 1;
-            }
-           else left = mid + 1;
-        } 
-        else {
-          if(target >= nums[mid] && target <= nums[right]) 
-            left = mid + 1;
-          else right = mid - 1;
+        unordered_map<int,vector<int>>mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            mpp[nums[i]].push_back(i);
         }
-          mid = left + (right - left) / 2;
-      }
-      return -1;
+
+        sort(nums.begin(),nums.end());
+        for(int i= 0 ; i < nums.size(); i++){
+            if(nums[i] == target){
+                if(mpp.find(nums[i]) != mpp.end()){
+                    return mpp[nums[i]][0];
+                }
+            }
+        }
+
+        return -1;
     }
 };
