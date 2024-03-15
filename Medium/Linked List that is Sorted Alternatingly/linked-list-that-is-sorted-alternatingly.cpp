4fabe1ac3ccd14a82ff822d377a1667e1b2dc6c6
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-15 09:45:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+    void sort(Node **head)
+    {
+        vector<int> ans;
+        Node *current = *head;
+        while(current != NULL){
+            ans.push_back(current->data);
+            current = current->next;
+        }
+        
+       std::sort(ans.begin(),ans.end());
+        
+        Node *dummyNode = new Node(-1);
+        Node *temp = dummyNode;
+        for(int i = 0; i < ans.size(); i++){
+            Node *newNode = new Node(ans[i]);
+            temp->next = newNode;
+            temp = newNode;
+        }
+        
+        *head = dummyNode->next;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
