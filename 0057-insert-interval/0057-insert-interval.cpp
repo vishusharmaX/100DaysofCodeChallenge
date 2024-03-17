930684d101585ce8d1191da0248a0ba66d96cf16
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-17 12:18:24 +0530

    Time: 8 ms (81.77%), Space: 20.4 MB (82.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        vector<vector<int>>result;
         int i = 0;
-
+        int n = intervals.size();
         while(i < intervals.size()){
             if(intervals[i][1] < newInterval[0]){
-                i++;
+                result.push_back(intervals[i]);
             }
             else if(intervals[i][0] > newInterval[1]){
-                intervals.insert(intervals.begin()+i,newInterval);
-                return intervals;
+                break;
             }
             else{
-                // merge kar do dono ko
                 newInterval[0] = min(intervals[i][0],newInterval[0]);
                 newInterval[1] = max(intervals[i][1],newInterval[1]);
-                intervals.erase(intervals.begin()+i);
             }
+            i++;
         }
 
-        intervals.push_back(newInterval);
-        return intervals;
+        result.push_back(newInterval);
+        while(i < n){
+            result.push_back(intervals[i]);
+            i++;
+        }
+        return result;
     }
 };
