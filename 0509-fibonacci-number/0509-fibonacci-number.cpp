2999a9718214d47a581d8613527a0715b41f4427
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-03-23 00:51:53 +0530

    Time: 19 ms (16.72%), Space: 5.9 MB (100%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
+
 public:
-    int solve(int n , vector<int> & dp){
-        if(n<=1) return n;
-
-        if(dp[n] != -1) 
-            return dp[n];
-
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
-    }
-
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-
-        return solve(n,dp);
-
+         if(n==0){
+            return 0 ;
+        }
+         if(n==1){
+            return 1;
+        }
+        //Recursive Relation 
+        int ans = fib(n-1) + fib(n-2);
+        return  ans;
     }
 };
