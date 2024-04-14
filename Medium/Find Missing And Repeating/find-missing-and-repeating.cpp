60ff156b8f73164933cbcd6dd617862e152e4b1f
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-04-14 22:55:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
@@ -6,49 +6,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    vector<int> findTwoElement(vector<int>& arr, int n) {
+    vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
-        // sort(arr.begin(), arr.end());
-        // unordered_map<int, int> mpp;
-        // int repeat = 0, missing = 0;
-
-        // for (int i = 0; i < n; i++) {
-        //     mpp[arr[i]]++;
-        // }
-
-        // for (auto it : mpp) {
-        //     if (it.second >= 2) {
-        //         repeat = it.first;
-        //         break;  // Exit the loop once the repeating element is found
-        //     }
-        // }
-
-        // for (int i = 0; i < n; i++) {
-        //     if (arr[i] != i + 1) {
-        //         missing = i + 1;
-        //         break;  // Exit the loop once the missing element is found
-        //     }
-        // }
-
-        // return {repeat, missing};
-        //  int n = a.size();
-    int hash[n + 1] = {0}; // Assuming hash is an array to keep track of occurrences
-
-    int repeating = -1, missing = -1;
-
-    for (int i = 0; i < n; ++i) {
-        if (hash[arr[i]] == 1)
-            repeating = arr[i];
-        else
-            hash[arr[i]] = 1;
-    }
-
-    for (int i = 1; i <= n; ++i) {
-        if (hash[i] == 0)
-            missing = i;
-    }
-
-    return {repeating, missing};
+        vector<int>ans(n+1,0);
+        
+        for(int i = 0 ; i <n ; i++){
+            ans[arr[i]]++;
+        }
+        
+        int missing  = -1 ,repeating  = -1;
+        
+        for(int i = 1; i <ans.size(); i++){
+            if(ans[i] == 2) repeating = i;
+            if(ans[i] == 0) missing = i;
+            
+            
+            if(missing != -1 && repeating != -1)
+                break;
+        }
+        
+        return {repeating,missing};
+        
     }
 };
 
