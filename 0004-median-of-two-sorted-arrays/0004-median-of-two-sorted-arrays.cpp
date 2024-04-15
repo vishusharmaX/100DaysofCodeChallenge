ae2a984c6e516b44a3c1f27b0d6550faf2a65122
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-04-16 01:36:01 +0530

    Time: 27 ms (50.88%), Space: 95.4 MB (29.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, vector<int>& nums2,vector<int> & ans){
-        int i = 0 , n = nums1.size();
-        int j = 0 , m = nums2.size();
-        while(i < n && j < m){
-            if(nums1[i] <= nums2[j]){
+    void solve(vector<int>&nums1,vector<int>& nums2,vector<int>&ans){
+
+        int i = 0, j = 0;
+        int n = nums1.size(), m = nums2.size();
+        while(i<n && j < m){
+            if(nums1[i] < nums2[j]){
                 ans.push_back(nums1[i]);
                 i++;
             }
-            
             else{
                 ans.push_back(nums2[j]);
                 j++;
-            }
+            }   
         }
         while(i<n){
             ans.push_back(nums1[i]);
             i++;
         }
+        
         while(j<m){
             ans.push_back(nums2[j]);
             j++;
         }
-
-        return;
-
+        
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
-        merge(nums1,nums2,ans);
-        double median = 0;
+
+        solve(nums1,nums2,ans);
+
         int n = ans.size();
-        if(n%2 == 0)  median = double(ans[n/2] + ans[n/2-1])/2;
-        else
-            median = ans[n/2];
-        
+        double median = 0;
+        if(n % 2 == 0){
+            median =  (ans[n/2 - 1] + ans[n/2]) / 2.0;
+        }
+        else {
+            median =  ans[n/2];
+        }
+
         return median;
     }
 };
