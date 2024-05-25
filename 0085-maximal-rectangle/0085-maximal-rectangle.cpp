deeff8b18b16821ad346d9f7381cda135d41379a
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-05-25 23:51:35 +0530

    Time: 35 ms (42.06%), Space: 23.8 MB (5.38%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,75 +1,71 @@
 class Solution {
 public:
-        vector<int>nextSmallerElement(vector<int>&arr,int n){
-            stack<int>st;
-            st.push(-1);
-            vector<int> ans(n);
-            for(int i = n-1 ; i >=0 ; i--){
-                int curr = arr[i];
-                while(st.top() != -1 &&  arr[st.top()] >= curr){
-                    st.pop();
-                }
-                
-                ans[i] = st.top();
-                st.push(i);
-            }
+vector<int> nextSmallerElement(vector<int>arr, int n){
+        stack<int>st;
+        st.push(-1);
+        vector<int>ans(n,0);
+        for(int i = n-1; i>=0 ; i--){
+            while( st.top() != -1 && arr[st.top()] >= arr[i])
+                st.pop();
             
-            return ans;
+            ans[i] = st.top();
+            st.push(i);
+        }
+        return ans;
+
     }
-     vector<int>prevSmallerElement(vector<int>&arr,int n){
-            stack<int>st;
-            st.push(-1);
-            vector<int> ans(n);
-            for(int i = 0 ; i < n  ; i++){
-                int curr = arr[i];
-                while(st.top() != -1 &&  arr[st.top()] >= curr){
-                    st.pop();
-                }
-                
-                ans[i] = st.top();
-                st.push(i);
-            }
+    vector<int> prevSmallerElement(vector<int>arr, int n){
+        stack<int>st;
+        st.push(-1);
+        vector<int>ans(n,0);
+        for(int i=0 ; i<n ; i++){
+            while( st.top() != -1 && arr[st.top()] >= arr[i])
+                st.pop();
             
-            return ans;
+            ans[i] = st.top();
+            st.push(i);
+        }
+        return ans;
+
     }
+
 
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
+        int n  = heights.size();
 
-        vector<int>next(n,0);
+        vector<int>next(n);
         next = nextSmallerElement(heights,n);
-        vector<int>prev(n,0);
+
+        vector<int>prev(n);
         prev = prevSmallerElement(heights,n);
-        int maxArea = INT_MIN;
-        for(int i = 0 ; i < n ; i++){
+        int maxi = 0;
+        for(int i = 0 ; i < n; i++){
             int l = heights[i];
+            if(next[i] == -1)
+                next[i] = n;
 
-            if(next[i] == -1){
-                next[i] = n ;
-            }
             int b = next[i] - prev[i] - 1;
-            int area = l * b;
 
-            maxArea = max(maxArea,area);
-
+            int area = l*b;
+            maxi = max(maxi,area);
         }
 
-        return maxArea;
+        return maxi;
+
     }
 
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<int>histogram(matrix[0].size(),0);
+        vector<int> histogram(matrix[0].size(), 0);
         int maxi = INT_MIN;
-        for(int i = 0 ; i < matrix.size(); i++){
-            for(int j = 0 ; j < histogram.size(); j++){
-                if(matrix[i][j] == '1'){
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < histogram.size(); j++) {
+                if (matrix[i][j] == '1') {
                     histogram[j]++;
-                }
-                else{
+                } else {
                     histogram[j] = 0;
                 }
             }
-            maxi = max(maxi,largestRectangleArea(histogram));
+            maxi = max(maxi, largestRectangleArea(histogram));
         }
 
         return maxi;
