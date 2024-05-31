c82e6e87bb43d421d2f82355d27d615ae8858b21
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-05-31 10:39:40 +0530

    Time: 13 ms (11.37%), Space: 13.4 MB (30.38%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         map<int,int>mpp;
-        vector<int> ans;
-        for(int i =0  ; i< nums.size(); i++){
-            mpp[nums[i]]++;
+        for(auto it : nums){
+            mpp[it]++;
         }
-        for(auto i : mpp){
-            if(i.second<=1){
-                ans.push_back(i.first);
-            }
+
+        vector<int>ans;
+        for(auto it : mpp){
+            if(it.second == 1)
+                ans.push_back(it.first);
         }
 
         return ans;
