0814fbe3cdf63b4f9d553d768ef6860944101e9e
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-06-07 18:46:17 +0530

    Time: 645 ms (17.09%), Space: 480.3 MB (10.34%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -4,21 +4,16 @@ public:
 
         for(int i = 1; i< word.size(); i++){
             string str = word.substr(0,i);
-            if(st.count(str))
-                return str;
+            if(st.count(str))return str;
         }
-
         return word;
-
     }
 
     string replaceWords(vector<string>& dictionary, string sentence) {
         unordered_set<string>st(begin(dictionary),end(dictionary));
-
         stringstream ss(sentence);
         string word;
         string result ;
-
         while(getline(ss,word,' ')){
             result += findRoot(st,word) + " ";
         }   
