981cd3b8ab0a2a662f3a049eaf95c5785b734ce9
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-06-08 22:30:25 +0530

    Time: 16 ms (14.51%), Space: 23.2 MB (30.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
+    vector<int>leftarray(vector<int>&height,int n){
+        vector<int>ans(n);
+        ans[0] = height[0];
+        for(int i = 1 ; i < n ; i++){
+            ans[i] = max(ans[i-1],height[i]);
+        }
+
+        return ans;
+
+    }
+    vector<int>rightarray(vector<int>&height,int n){
+        vector<int>ans(n);
+        ans[n-1] = height[n-1];
+        for(int i = n-2 ; i >=0 ; i--){
+            ans[i] = max(ans[i+1],height[i]);
+        }
+
+        return ans;
+
+    }
     int trap(vector<int>& height) {
         int n = height.size();
-        int i = 0;
-        int j = n - 1;
-        int prefix = 0;
-        int suffix = 0;
+
+        vector<int>leftArray = leftarray(height,n);
+        vector<int>RightArray = rightarray(height,n);
+
         int ans = 0;
-
-        while (i < j) {
-            prefix = max(prefix, height[i]);
-            suffix = max(suffix, height[j]);
-
-            if (prefix <= suffix) {
-                ans += prefix - height[i];
-                i++;
-            } else {
-                ans += suffix - height[j];
-                j--;
-            }
+        for(int i = 0 ; i < n; i++){
+            ans += min(leftArray[i],RightArray[i])- height[i];
         }
 
         return ans;
