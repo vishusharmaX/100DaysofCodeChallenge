d573ddd8f5946ad5ee3cb41be78aa6761f9b6931
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-06-13 01:34:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (11.96%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,23 @@
-
-int maxAdjacent(vector<int>&nums){
-
-    int  n = nums.size();
-
-    int prev = nums[0];
-    int prev2 = 0;
-
-    for(int i= 0;i<n ; i++){
-        int take = nums[i];
-
-        if(i>1) take +=prev2;
-
-        int nottake = 0 + prev;
-
-        int curr = max(take,nottake);
-
-        prev2= prev;
-        prev = curr;
-    }
-
-    return prev;
-}
-
 class Solution {
 public:
+    int solve(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        vector<int> dp(n, 0);
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            int include = dp[i-2] + arr[i];
+            int exclude = dp[i-1];
+            dp[i] = max(include, exclude);
+        }
+
+        return dp[n-1];
+    }
+        
     int rob(vector<int>& nums) {
         vector<int> temp1,temp2;
         int n = nums.size();
@@ -35,6 +29,6 @@ public:
             if(i!=n-1)temp2.push_back(nums[i]);
         }
 
-        return max(maxAdjacent(temp1), maxAdjacent(temp2));
+        return max(solve(temp1),solve(temp2));
     }
 };
