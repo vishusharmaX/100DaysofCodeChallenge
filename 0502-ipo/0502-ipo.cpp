8b374ce952d52c0153945e5e60ef1b762103378f
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-06-16 11:34:25 +0530

    Time: 131 ms (85.44%), Space: 85.6 MB (27.14%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         int  n = profits.size();
-        int ans =  0;
+
         vector<pair<int,int>>v;
         for(int i = 0 ; i < n; i++){
             v.push_back({capital[i],profits[i]});
-            ans +=profits[i];
-        }
-        if(k == n){
-           return ans;
+
         }
 
         sort(v.begin(),v.end());
@@ -33,39 +30,6 @@ public:
         }
 
         return currcapital;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int i = 0, totalworth = w;
-        // while(k--){
-        //     int cap = v[i].first;
-        //     int profit = v[i].second;
-
-        //     if(cap > totalworth+profit){
-        //         return 0;
-        //     }
-
-        //     else if(cap < totalworth + profit){
-        //         totalworth += profit ;
-        //         i++;
-        //     }
-        // }
-        
-        // ans = totalworth;
-
-
-        return ans;
 
     }
 };
