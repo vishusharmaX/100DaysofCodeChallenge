160756a0079e0e4fa9cc5e7e1284350d90eeca16
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-06-26 12:30:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findCoverage(vector<vector<int>>& matrix) {
+        // Code here
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 1)
+                    continue;
+                // cout << i << " " << j << endl;
+                if(j > 0){
+                    if(matrix[i][j-1] == 1){
+                        count++;
+                        // cout << "LEFT" << " ";
+                    }
+                }
+                if(i > 0){
+                    if(matrix[i-1][j] == 1){
+                        count++;
+                        // cout << "TOP" << " ";
+                    }
+                }
+                if(j < m-1){
+                    if(matrix[i][j+1] == 1){
+                        count++;
+                        // cout << "RIGHT" << " ";
+                    }
+                }
+                if(i < n-1){
+                    if(matrix[i+1][j] == 1){
+                        count++;
+                        // cout << "DOWN";
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> matrix(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> matrix[i][j];
+        Solution obj;
+        int ans = obj.findCoverage(matrix);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
