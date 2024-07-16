80d8e5c5872b0384595161228680671988de581e
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-07-16 14:49:31 +0530

    Time: 24 ms (47.48%), Space: 30 MB (6.23%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string s, string t) {
+        
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i = 1 ; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+
+        int i = n , j = m ;
+        string ans ;
+        while( i > 0 && j > 0){
+            if(s[i-1] == t[j-1]){
+                ans = s[i-1] + ans;
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i][j-1] > dp[i-1][j]){
+                    ans = t[j-1] + ans;
+                    j--;
+                }
+                else{
+                    ans = s[i-1] + ans;
+                    i--;
+                }
+            }
+        }
+
+        while( i > 0){
+           ans = s[i-1] + ans;
+            i--;
+        }
+        while( j > 0){
+           ans = t[j-1] + ans;
+            j--;
+        }
+
+        return ans;
+        
+    }
+};
