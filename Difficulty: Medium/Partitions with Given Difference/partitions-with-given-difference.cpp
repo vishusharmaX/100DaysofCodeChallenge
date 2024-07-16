a5d3310520efd0f32f09542bf7adeb6835e530ec
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-07-16 19:55:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -3,14 +3,27 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution {
   public:
-    const int mod = 1000000007;
-    void getCountSubset(vector<int>&arr, int n , int sum , vector<vector<int>>&dp){
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int total_sum = 0;
+        for(auto it : arr){
+            total_sum+=it;
+        }
         
+        
+        
+        int sum = (d+total_sum)/2;
+        
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+        if ((total_sum + d) % 2 != 0) return 0;
+        for(int i = 0; i < n+1; i++){
+            dp[i][0] = 1;
+        }
+        const int mod = 1000000007;
         for(int i = 1; i < n+1; i++){
-            for(int j = 0 ; j < sum+1; j++){
+            for(int j= 0; j<sum+1; j++){
                 if(arr[i-1] <= j){
                     dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j])%mod;
                 }
@@ -18,29 +31,12 @@ class Solution {
                     dp[i][j] = dp[i-1][j];
                 }
             }
-        }    
-    }
-    
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int total_sum = 0;
-        for(int i = 0 ; i < n; i++){
-            total_sum += arr[i];
         }
-        if ((total_sum + d) % 2 != 0) return 0;
-        int sum = (total_sum + d)/2;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
-        
-        for(int i= 0; i  < n+1; i++){
-            dp[i][0] = 1;
-        }
-        
-        getCountSubset(arr,n,sum,dp);
         
         return dp[n][sum];
+        
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
