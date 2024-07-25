8f4848f9d00e01c025e1e7b648a96135b01424fe
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-07-26 02:26:40 +0530

    Time: 3 ms (29.47%), Space: 7.5 MB (14.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-        if(n <= 1)
-            return 1 ;
-        if(dp[n]!=-1)
+    int solve(int n , vector<int>&dp){
+        if(n<=1)
+            return 1;
+        
+        if(dp[n] != -1)
             return dp[n];
-        int onestep =  solve(n-1,dp);
-        int twostep =  solve(n-2,dp);
+        
+        int onestep = solve(n-1,dp);
+        int twostep = solve(n-2,dp);
 
         return dp[n] = onestep+twostep;
     }
+
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
+
         return solve(n,dp);
     }
 };
