8b35b238662d8cec6a20447819f08ed4bbf63156
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-07-27 12:24:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -1,25 +1,26 @@
 //{ Driver Code Starts
 //Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
 
-class Solution{   
-public:
-    int findMinInsertions(string s){
-        // code here 
+class Solution{
+  public:
+    int countMin(string s){
+    //complete the function here
+        int n = s.length();
         string t = s;
         reverse(t.begin(),t.end());
-        int n = s.length() , m = t.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // cout<<t<<endl;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
         for(int i = 1; i < n+1; i++){
-            for(int j = 1; j <m+1; j++){
+            for(int j = 1; j  < n+1; j++){
                 if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1 +  dp[i-1][j-1];
                 }
                 else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
@@ -27,8 +28,7 @@ public:
             }
         }
         
-        int ans = n -dp[n][m];
-        
+        int ans = n - dp[n][n];
         return ans;
         
     }
@@ -36,19 +36,17 @@ public:
 
 
 //{ Driver Code Starts.
-
-
-
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
-        string S;
-        cin>>S;
+        string str;
+        cin >> str;
         Solution ob;
-        cout<<ob.findMinInsertions(S)<<endl;
+        cout << ob.countMin(str) << endl;
     }
-    return 0;
+return 0;
 }
+
 
 // } Driver Code Ends
