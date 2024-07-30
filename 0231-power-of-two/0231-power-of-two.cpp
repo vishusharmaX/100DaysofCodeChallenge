91de4b0a57452bdfb5adec4f3124050f2fd091b5
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-07-30 13:04:52 +0530

    Time: 0 ms (100%), Space: 7.2 MB (54.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,10 +3,6 @@ public:
     bool isPowerOfTwo(int n) {
         if(n == 0 || n < 0)
             return false;
-        int ans = n & n-1;
-        if(ans == 0)
-            return true;
-        
-        return false;
+        return n && !(n &(n-1));
     }
 };
