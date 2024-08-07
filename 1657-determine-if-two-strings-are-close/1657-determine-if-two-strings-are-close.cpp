9da825b1f23e78943ccbc2719fa0daea0fef6bf6
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-08-08 02:08:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
-    bool closeStrings(string word1, string word2) {
+    bool closeStrings(string s, string t) {
+
+        bool ans = true;
+        map<char, int> mpp1, mpp2;
+        if(s.length() != t.length()){
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            mpp1[s[i]]++;
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            mpp2[t[j]]++;
+        }
+        vector<int> ans1, ans2;
+
+        for (auto it : mpp1) {
+            ans1.push_back(it.second);
+        }
+        for (auto it : mpp2) {
+            ans2.push_back(it.second);
+        }
+        sort(ans1.begin(), ans1.end());
+        sort(ans2.begin(), ans2.end());
+        int i = 0, j = 0;
+        if(ans1.size() != ans2.size())
+            return false;
+        while( i < ans1.size() && j < ans2.size()){
+            if(s[i] != s[j]){
+                return  false;
+            }
+            i++;
+            j++;
+        }
         
-        vector<int> v1(26) ,v2(26),v11(26,0),v22(26,0);
-
-        for(auto it : word1)v1[it-'a']++,v11[it-'a']=1;
-        for(auto it : word2)v2[it-'a']++,v22[it-'a']=1;
-
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-
-        return v1==v2 && v11==v22;
-
-
+        return ans;
     }
 };
