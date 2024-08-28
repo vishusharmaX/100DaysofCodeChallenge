b131f949f2b703519d8f14ed23e682fb15c15bf7
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-08-29 04:01:38 +0530

    Time: 93 ms (60.64%), Space: 79.5 MB (33.99%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
+    vector<vector<string>>ans;
+    bool isPallindrome(string s){
+
+        int i = 0, j = s.length()-1;
+        while(i <= j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+
+    }
+
+    void solve(string &s,vector<string>&temp, int index){
+
+        if(index == s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        string pat;
+        for(int i = index; i < s.size(); i++){
+            pat+=s[i];
+            if(isPallindrome(pat)){
+                temp.push_back(pat);
+                solve(s,temp,i+1);
+                temp.pop_back();
+            }
+        }
+
+
+    }
+
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        int n = s.size();
-
-        auto isPalindrome = [](const string& str) {
-            int left = 0, right = str.size() - 1;
-            while (left < right) {
-                if (str[left] != str[right]) return false;
-                left++;
-                right--;
-            }
-            return true;
-        };
-
-        function<void(int, vector<string>&)> getAllPartitions = [&](int curInd, vector<string>& curList) {
-            if (curInd == n) {
-                if (isPalindrome(curList.back())) {
-                    res.push_back(curList);
-                }
-                return;
-            }
-            if (isPalindrome(curList.back())) {
-                curList.push_back(string(1, s[curInd]));
-                getAllPartitions(curInd + 1, curList);
-                curList.pop_back();
-            }
-            curList.back().push_back(s[curInd]);
-            getAllPartitions(curInd + 1, curList);
-            curList.back().pop_back();
-        };
-
-        vector<string> initial = { string(1, s[0]) };
-        getAllPartitions(1, initial);
-        return res;
+        vector<string>temp;
+        solve(s,temp,0);
+        return ans;    
     }
 };
