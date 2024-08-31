f2973cca10ec398ae670a445b372cbf5ccce99f4
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-08-31 14:33:10 +0530

    Time: 16 ms (33.17%), Space: 8 MB (66.91%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,47 +1,45 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board) {
-
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-
-                if (board[i][j] == '.') {
-
-                    for (char ch = '1'; ch <= '9'; ch++) {
-                        if (isValid(board, i, j, ch)) {
-                            board[i][j] = ch;
-
-                            if (solve(board) == true)
-                                return true;
-                            else
-                                board[i][j]  = '.';
-                        }
-                    }
-                    return false;
-                }
+    bool issafe(int row, int col, vector<vector<char>>& board, char val) {
+        int n = board.size();
+        for (int i = 0; i < n; i++) {
+            if (board[row][i] == val) {
+                return false;
+            }
+            if (board[i][col] == val) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == val) {
+                return false;
             }
         }
         return true;
     }
-    bool isValid(vector<vector<char>>& board , int row, int  col , char c){
-        for(int  i = 0 ; i < 9; i++){
-            if(board[i][col] == c)
-                return false;
 
-            if(board[row][i] == c)
-                return false;
-            
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c)
-                return false;
+    bool solve(vector<vector<char>>& board) {
+        int n = board.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {  
+                    for (char val = '1'; val <= '9'; val++) {
+                        if (issafe(i, j, board, val)) {
+                            board[i][j] = val;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.'; 
+                            }
+                        }
+                    }
+                    return false; 
+                }
+            }
         }
-
-        return true;
-
-
+        return true;  
     }
 
-
-    void solveSudoku(vector<vector<char>>& board) { 
-        solve(board); 
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
 };
