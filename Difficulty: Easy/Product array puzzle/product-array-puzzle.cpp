806c40e8b9af2ee8a7e84acc832d6b57f216723d
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-09-22 12:59:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -14,19 +14,32 @@ class Solution {
     // return the Product vector P that hold product except self at each index
     vector<long long int> productExceptSelf(vector<long long int>& nums) {
 
-        vector<long long int>ans;
+        // code here
+        int n = nums.size();
+        vector<long long int>ans(n);
+        vector<long long int>left(n);
+        vector<long long int>right(n);
         
-        for(int i = 0; i < nums.size(); i++){
-            long long int product = 1;
-            for(int j = 0; j < nums.size(); j++){
-                if(i !=j){
-                    product *= nums[j];
-                }
-            }
-            ans.push_back(product);
+        left[0] = 1;
+        right[n-1] = 1;
+        
+        for(int i = 1; i < n; i++){
+            left[i] = left[i-1]*nums[i-1];
         }
         
+        for(int j = n-2; j >=0; j--){
+            right[j] = right[j+1]*nums[j+1];
+        }
+        
+        
+        for(int i = 0; i < n; i++){
+            ans[i] = left[i] * right[i];
+        }
+        
+        
         return ans;
+        
+        
     }
 };
 
