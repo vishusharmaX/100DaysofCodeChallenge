7da4cd60283ef7029f7bd11f51ea8df8c0e9aa9e
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-04 21:51:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,67 +1,71 @@
 //{ Driver Code Starts
-//Initial template for C++
-
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-class Solution
-{
+class Solution {
   public:
-    //Function to find maximum of each subarray of size k.
-    vector <int> max_of_subarrays(int *arr, int n, int k)
-    {
-        // your code here
-        vector<int>ans;
-        deque<int>dq;
+    // Function to find maximum of each subarray of size k.
+    vector<int> max_of_subarrays(int k, vector<int> &arr) {
+        int n = arr.size();
+        int i = 0, j = 1, maxi = 0;
+        vector<int> ans;
         
-        for(int i = 0 ;  i < n;i++){
-            
-            if(!dq.empty() && dq.front() == i-k){
-                dq.pop_front();
-            }
-            
-            while(!dq.empty() && arr[i] >= arr[dq.back()])
-                dq.pop_back();
-                
-            dq.push_back(i);
-            
-            if(i >= k-1){
-                ans.push_back(arr[dq.front()]);
-            }
-            
+        while(i < k) {
+            if(arr[i] > maxi) maxi = arr[i];
+            i++;
+        }
+        ans.push_back(maxi);
+        
+        while(i < n) {
+            if(arr[j-1]==maxi)
+                maxi = *max_element(arr.begin()+j,arr.begin()+i+1);
+            else if(arr[i]>maxi) 
+                maxi = arr[i];
+            ans.push_back(maxi);
+            i++; j++;
         }
         
         return ans;
+        
     }
+
+
 };
 
 //{ Driver Code Starts.
 
 int main() {
-	
-	int t;
-	cin >> t;
-	
-	while(t--){
-	    
-	    int n, k;
-	    cin >> n >> k;
-	    
-	    int arr[n];
-	    for(int i = 0;i<n;i++) 
-	        cin >> arr[i];
-	    Solution ob;
-	    vector <int> res = ob.max_of_subarrays(arr, n, k);
-	    for (int i = 0; i < res.size (); i++) 
-	        cout << res[i] << " ";
-	    cout << endl;
-	    
-	}
-	
-	return 0;
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> res = obj.max_of_subarrays(k, arr);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
 }
+
 // } Driver Code Ends
