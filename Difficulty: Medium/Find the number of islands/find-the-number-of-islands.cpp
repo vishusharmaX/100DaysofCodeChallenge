2e180ce3c90d21afdf6c0f57b3491767b92d8616
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-06 05:25:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
 class Solution {
   public:
     // Function to find the number of islands.
@@ -10,38 +12,39 @@ class Solution {
         // Code here
         int n = grid.size();
         int m = grid[0].size();
-        int cnt = 0;
+        
         queue<pair<int,int>>q;
+        int cnt = 0;
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j< m; j++){
-                if(grid[i][j]== '1'){
-                    cnt++;
+            for(int j = 0;j < m; j++){
+                if(grid[i][j] == '1'){
                     q.push({i,j});
                     grid[i][j] = '0';
+                    cnt++;
                     while(!q.empty()){
                         int x = q.front().first;
                         int y = q.front().second;
                         q.pop();
+                        int row[] = {-1,-1,-1,1,1,1,0,0};
+                        int col[] = {-1,0,1,-1,0,1,-1,1};
                         
-                        int dirx[] = {-1,-1,-1,0,0,1,1,1};
-                        int diry[] = {-1,0,1,-1,1,-1,0,1};
-                        
-                        for(int k = 0; k <8; k++){
-                            int newx = x + dirx[k];
-                            int newy = y + diry[k];
+                        for(int k = 0; k< 8; k++){
+                            int newx = x + row[k];
+                            int newy = y + col[k];
                             
-                            if(newx>=0  && newx < n && newy >=0 && newy < m && grid[newx][newy] =='1' ){
+                            if(newx >= 0 && newx < n && newy >= 0 && newy < m && grid[newx][newy] == '1'){
                                 q.push({newx,newy});
                                 grid[newx][newy] = '0';
                             }
                         }
-                        
                     }
                 }
             }
         }
         
         return cnt;
+        
     }
 };
 
