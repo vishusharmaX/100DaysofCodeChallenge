d991f510fab19c33fe66dbda10506c47395b2d44
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-10 12:39:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.cpp
+++ Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.cpp
@@ -8,18 +8,18 @@ class Solution {
   public:
     int maxDistance(vector<int> &arr) {
         // Code here
-        int n = arr.size();
+        int maxi = 0;
         map<int,int>mpp;
-        for(int i = 0; i < n; i++){
-            mpp[arr[i]] = i;
+        for(int i = 0; i < arr.size(); i++){
+            if(mpp.find(arr[i]) != mpp.end()){
+                int pos = mpp[arr[i]];
+                maxi = max(maxi,i-pos);
+            }
+            else{
+                mpp[arr[i]] = i;
+            }
         }
         
-        int maxi = INT_MIN;
-        for(int i = 0; i <arr.size(); i++){
-           if(mpp.find(arr[i]) != mpp.end()){
-               maxi = max(maxi,mpp[arr[i]]-i);
-           }
-        }
         
         return maxi;
     }
