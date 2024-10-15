e3db3f01d12af5e9f535608584596107c5a73e93
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-15 19:55:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
@@ -6,28 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int lenOfLongSubarr(int arr[],  int n, int k) 
+    int lenOfLongSubarr(int A[],  int N, int K) 
     { 
         // Complete the function
-         map<int,int>mpp;
-        int sum = 0 , maxlen = 0 ;
-        mpp[0] = 1;
-        for(int i = 0 ; i < n; i++){
-            sum += arr[i];
-            if(sum == k)
-                maxlen = max(maxlen,i+1);
-            long long rem = sum - k;
-            if(mpp.find(rem) != mpp.end()){
-                int len = i - mpp[rem];
-                maxlen = max(maxlen,len);
+        map<int,int>mpp;
+        int sum = 0;
+        int maxlen = 0;
+        for(int i = 0; i < N; i++){
+            sum += A[i];
+            if(sum == K){
+                maxlen =max(maxlen, i+1);
             }
-            if(mpp.find(sum)== mpp.end()){
+            int left = sum - K;
+            if(mpp.find(left) != mpp.end()){
+                maxlen = max(maxlen,i-mpp[left]);
+            }
+            
+            if(mpp.find(sum) == mpp.end()){
                 mpp[sum] = i;
             }
         }
-
-        return maxlen;
         
+        return maxlen;
     } 
 
 };
