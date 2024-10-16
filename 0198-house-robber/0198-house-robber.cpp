036a4ee96b7146d269cc5860f2cd436fb2f6486c
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-16 19:45:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (62.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int indx,vector<int>&dp){
-
-        if(indx < 0){
+    int solve(vector<int>&nums, int indx,vector<int>&dp){
+        if(indx >= nums.size())
             return 0;
-        }
-        if(indx  == 0)
-            return nums[0];
         
         if(dp[indx] != -1)
             return dp[indx];
 
-        int include = solve(nums,indx-2,dp) + nums[indx];
-        int exclude = solve(nums,indx-1,dp) + 0 ;
+        int take = nums[indx] + solve(nums,indx+2,dp);
+        int nottake = solve(nums,indx+1,dp);
 
-        return dp[indx] = max(include,exclude);
+        return dp[indx] = max(take,nottake);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return solve(nums,n-1,dp);
+        vector<int>dp(n+1,0);
+        // return solve(nums,0,dp);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i <=n; i++){
+            dp[i] = max((nums[i-1] + dp[i-2]) , dp[i-1]);
+        }
 
+        return dp[n];
     }
 };
