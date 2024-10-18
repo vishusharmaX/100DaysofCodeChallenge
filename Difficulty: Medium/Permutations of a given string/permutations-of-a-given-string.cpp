7249f8e5fe5631f8446969d8cc9a583f941c26d3
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-10-19 01:25:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a given string/permutations-of-a-given-string.cpp
+++ Difficulty: Medium/Permutations of a given string/permutations-of-a-given-string.cpp
@@ -1,59 +1,52 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-	public:
-	    void solve(string &s,int n, int indx, vector<string>&ans){
-	        
-	        if(indx >= n-1){
-	            ans.push_back(s);
-	            return;
-	        }
-	        set<char>st;
-	        for(int i = indx ; i < n; i++){
-	            if(st.find(s[i]) == st.end()){
-	                st.insert(s[i]);
-	                swap(s[indx],s[i]);
-	                solve(s,n,indx+1,ans);
-	                swap(s[indx],s[i]);
-	            }
-	        }
-	        
-	    }
-	    
-		vector<string>find_permutation(string S)
-		{
-		    // Code here there
-		     int  n = S.size();
-		    vector<string>ans;
-		    solve(S,n,0,ans);
-		    return ans;
-		}
+class Solution {
+  public:
+    void solve(string s, int idx, vector<string>&ans){
+        if(idx>= s.size()-1){
+            ans.push_back(s);
+            return;
+        }
+        set<char>st;
+        for(int i = idx; i<s.size(); i++){
+            if(st.find(s[i]) == st.end()){
+                st.insert(s[i]);
+                swap(s[idx],s[i]);
+                solve(s,idx+1,ans);
+                swap(s[idx],s[i]);
+            }
+        }
+    }
+    vector<string> find_permutation(string s) {
+        // Code here there
+        vector<string>ans;
+        
+        solve(s,0,ans);
+        return ans;
+    }
 };
 
 
-
 //{ Driver Code Starts.
-int main(){
+int main() {
     int t;
     cin >> t;
-    while(t--)
-    {
-	    string S;
-	    cin >> S;
-	    Solution ob;
-	    vector<string> ans = ob.find_permutation(S);
-	    sort(ans.begin(),ans.end());
-	    for(auto i: ans)
-	    {
-	    	cout<<i<<" ";
-	    }
-	    cout<<"\n";
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        vector<string> ans = ob.find_permutation(S);
+        sort(ans.begin(), ans.end());
+        for (auto i : ans) {
+            cout << i << " ";
+        }
+        cout << "\n";
     }
-	return 0;
+    return 0;
 }
 
 // } Driver Code Ends
