a86beab6bb333eefdf46fbb6f800bc1cd2cce02c
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-11-10 00:36:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
@@ -8,9 +8,32 @@ using namespace std;
 // x: element to find square root
 class Solution {
   public:
-    long long int floorSqrt(long long int n) {
+    int floorSqrt(int n) {
         // Your code goes here
-        return sqrt(n);
+        
+        int low = 1;
+        int high =n;
+        int ans = -1;
+        
+        while(low <= high){
+            int mid = ( low + high )/2;
+            
+            int sqr = mid*mid;
+            
+            if(sqr == n){
+                return mid;
+            }
+            else if(sqr < n){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+            
+        }
+        
+        return ans;
     }
 };
 
@@ -24,6 +47,9 @@ int main() {
         cin >> n;
         Solution obj;
         cout << obj.floorSqrt(n) << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
