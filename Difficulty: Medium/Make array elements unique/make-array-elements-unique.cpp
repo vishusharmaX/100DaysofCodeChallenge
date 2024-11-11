121588b1b87c91979d21d29d5fafa9e7b05fbf39
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-11-11 23:19:44 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make array elements unique/make-array-elements-unique.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minIncrements(vector<int>& arr) {
+        int N = arr.size();
+        sort(arr.begin(),arr.end());
+         int ans = 0;
+        for(int i = 1; i < N; i++){
+            if(arr[i-1] >= arr[i]){
+                ans += arr[i-1] - arr[i] + 1;
+                arr[i] = arr[i-1] +1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.minIncrements(a);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
