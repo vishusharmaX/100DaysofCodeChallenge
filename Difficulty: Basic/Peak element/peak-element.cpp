8955ae2142a7242b7e27dbad907454530e9f0987
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2024-12-15 11:47:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.cpp
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -1,20 +1,15 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-/* The function should return the index of any
-   peak element present in the array */
 
-// arr: input array
-// n: size of array
-class Solution
-{
-    public:
-    int peakElement(int arr[], int n)
-    {
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
         // Your code here
+        int n = arr.size();
         int low = 0 ;
         int high = n -1;
         
@@ -36,46 +31,47 @@ class Solution
     }
 };
 
-
 //{ Driver Code Starts.
 
 int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int a[n], tmp[n];
-		for(int i=0;i<n;i++)
-		{
-			cin>>a[i];
-			tmp[i] = a[i];
-		}
-		bool f=0;
-		Solution ob;
-		
-		int A = ob. peakElement(tmp,n);
-		
-		if(A<0 and A>=n)
-		    cout<<0<<endl;
-		else
-		{
-    		if(n==1 and A==0)
-    		    f=1;
-    		else if(A==0 and a[0]>=a[1])
-    		    f=1;
-    		else if(A==n-1 and a[n-1]>=a[n-2])
-    		    f=1;
-    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
-    		    f=1;
-    		else
-    		    f=0;
-    		cout<<f<<endl;
-		}
-		
-	}
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
 
-	return 0;
+        Solution ob;
+        int idx = ob.peakElement(a);
+        int n = a.size();
+        bool f = 0;
+        if (idx < 0 and idx >= n)
+            cout << "false" << endl;
+        else {
+            if (n == 1 and idx == 0)
+                f = 1;
+            else if (idx == 0 and a[0] > a[1])
+                f = 1;
+            else if (idx == n - 1 and a[n - 1] > a[n - 2])
+                f = 1;
+            else if (a[idx] > a[idx + 1] and a[idx] > a[idx - 1])
+                f = 1;
+            else
+                f = 0;
+            if (f)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
 }
+
 // } Driver Code Ends
