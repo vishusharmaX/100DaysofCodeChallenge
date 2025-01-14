2c8de5c3f5bef8896bdc9c345917011e998e1f17
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-01-14 16:44:58 +0530

    Time: 0 ms (100%), Space: 85.9 MB (76.89%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int>freq(n+1,0);
+        vector<int>res(n,0);
+        int cnt = 0;
+        
+        for(int i = 0; i < n; i++){
+            freq[A[i]]++;
+            if(freq[A[i]] == 2) cnt++;
+            freq[B[i]]++;
+            if(freq[B[i]] == 2) cnt++;
+            
+            res[i] = cnt;
+        }
+
+        return res;
+    }
+};
