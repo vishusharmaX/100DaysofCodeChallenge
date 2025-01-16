7fc7c97af30864d799e06cd32e261e59432df1b9
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-01-16 16:06:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
@@ -4,38 +4,32 @@ using namespace std;
 
 
 // } Driver Code Ends
-/*You are required to complete this method*/
 
-// arr[] : the input array containing 0s and 1s
-// N : size of the input array
 
-// return the maximum length of the subarray
-// with equal 0s and 1s
-class Solution{
+class Solution {
   public:
-    int maxLen(int arr[], int N)
-    {
-        for(int i=0;i<N;i++){
-            if(arr[i]==0)
-            arr[i]= -1;
-        }
-        
-        unordered_map<int,int>mpp;
-        int sum =  0;
-        int maxlen = 0;
+    int maxLen(vector<int> &arr) {
+        // Your code here
+        int maxlen = 0 ;
+        int sum = 0;
+        map<int,int>mpp;
         mpp[0] = -1;
-        for(int i = 0 ; i < N; i++){
-            sum += arr[i];
-            
-            if(mpp.find(sum)  != mpp.end()){
-                maxlen = max(maxlen, i - mpp[sum]);
+        for(int i = 0;i < arr.size(); i++){
+            if(arr[i] == 0){
+                sum += -1;
+            }
+            else{
+                sum += 1;
+            }
+            if(mpp.find(sum) != mpp.end()){
+                int len = i - mpp[sum];
+                maxlen = max(maxlen,len);
             }
             
             else{
                 mpp[sum] = i;
             }
         }
-        
         
         return maxlen;
     }
@@ -44,21 +38,30 @@ class Solution{
 
 //{ Driver Code Starts.
 
-int main()
-{
-    // your code goes here
+int main() {
     int T;
     cin >> T;
-    while (T--)
-    {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
+    cin.ignore(); // To ignore the newline character after reading T
+
+    while (T--) {
+        string line;
+        getline(cin, line); // Read the whole line for the array
+
+        // Convert the line into an array of integers
+        stringstream ss(line);
+        vector<int> a;
+        int num;
+        while (ss >> num) {
+            a.push_back(num);
+        }
+
+        // Create the solution object
         Solution obj;
-        cout << obj.maxLen(a, n) << endl;
+
+        // Call the maxLen function and print the result
+        cout << obj.maxLen(a) << endl;
     }
+
     return 0;
 }
 // } Driver Code Ends
