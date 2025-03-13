a3288f1dbc921ae4deeb327dadffe0d0c061d4b8
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-03-14 02:45:14 +0530

    Time: 812 ms (5.11%), Space: 45.6 MB (29.4%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-       vector<int>temp = arr;
-       sort(arr.begin(),arr.end());
-       map<int,int>mpp;
-       int rank = 1;
-      
-        for(int i = 0; i <arr.size(); i++){
-            if(mpp.find(arr[i]) == mpp.end()){
-                mpp[arr[i]] = rank;
-                rank++;
-            }
+    vector<int>arr1(arr.begin(),arr.end());
+    sort(arr1.begin(),arr1.end());
+    map<int,int>mpp;
+    int j = 1;
+    for(int i =0; i < arr1.size(); i++){
+        if(mpp.find(arr1[i]) == mpp.end()){
+            mpp[arr1[i]] = j++;
         }
+    }
+    
+    for(auto it : mpp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+    
+    
+    vector<int>ans;
+    
+    for(int i = 0; i < arr.size(); i++){
+        int num = mpp[arr[i]];
+        cout<<num<<endl;
+        ans.push_back(num);
+    }
 
-        vector<int>ans;
-        for(int i = 0; i<temp.size(); i++){
-            ans.push_back(mpp[temp[i]]);
-        }
-
-        return ans;
-
-        return arr;
-
+    return ans;
     }
 };
