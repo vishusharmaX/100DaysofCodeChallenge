c1d8ead59abe9bb25e29d20555031571748b7f44
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-07 18:00:07 +0530

    Time: 1 ms (25.91%), Space: 27.5 MB (6.23%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    vector<int>leftarray(vector<int>&height,int n){
-        vector<int>ans(n);
-        ans[0] = height[0];
-        for(int i = 1 ; i < n ; i++){
-            ans[i] = max(ans[i-1],height[i]);
+    vector<int> solve(vector<int>& arr){
+        vector<int>newarr(arr.size(), 0);
+        int maxi = arr[0];
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+            newarr[i] = maxi;
         }
-
-        return ans;
-
+        return newarr;
     }
-    vector<int>rightarray(vector<int>&height,int n){
-        vector<int>ans(n);
-        ans[n-1] = height[n-1];
-        for(int i = n-2 ; i >=0 ; i--){
-            ans[i] = max(ans[i+1],height[i]);
+    vector<int> solve1(vector<int>& arr){
+        vector<int>newarr(arr.size(), 0);
+        int maxi = arr[arr.size()-1];
+        for(int i = arr.size()-2; i >=0 ; i--){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+            newarr[i] = maxi;
         }
-
-        return ans;
-
+        return newarr;
     }
-    int trap(vector<int>& height) {
-        int n = height.size();
 
-        vector<int>leftArray = leftarray(height,n);
-        vector<int>RightArray = rightarray(height,n);
-
-        int ans = 0;
-        for(int i = 0 ; i < n; i++){
-            ans += min(leftArray[i],RightArray[i])- height[i];
+    int trap(vector<int>& arr) {
+        vector<int>leftmax = solve(arr);
+        vector<int>rightmax = solve1(arr);
+        int cnt = 0;
+        for(int i = 0; i < arr.size(); i++){
+            int h = min(leftmax[i] , rightmax[i]);
+            int w = arr[i];
+            int a = h-w;
+            if(a >= 0){
+                cnt += a;
+            }
         }
 
-        return ans;
+        return cnt;
     }
 };
