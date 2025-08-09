50952b99260ca7ca10984032dfc984b2889faf53
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-10 00:25:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -1,98 +1,63 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    
-    bool valid(vector<vector<int>>& mat, int val, int x, int y) {
-        for(int j = 0; j < mat[0].size(); j++) if(mat[x][j] == val) return false;
-        for(int i = 0; i < mat.size(); i++) if(mat[i][y] == val) return false;
+    // Function to find a solved Sudoku.
+    bool issafe(vector<vector<int>>&mat,int row, int col, int digit){
         
-        x = 3*(x/3), y = 3*(y/3);
         
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                if(mat[x + i][y + j] == val) return false;
+        for(int  i = 0; i <=8; i++){
+            if(mat[i][col] == digit) return false;
+        }
+        for(int  i = 0; i <=8; i++){
+            if(mat[row][i] == digit) return false;
+        }
+        
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        
+        for(int i = sr; i < sr +3;i++){
+            for(int j =sc ; j < sc +3;j++ ){
+                if(mat[i][j] == digit){
+                    return false;
+                }
             }
         }
+        
         
         return true;
     }
-    
-    bool solve(vector<vector<int>>& mat, vector<pair<int, int>>& to_fill, int index) {
-        if(index >= to_fill.size()) return true;
+
+    bool solve(vector<vector<int>>&mat, int row, int col){
+        if(row == 9){
+            return true;
+        }
+        int nextrow = row, nextcol = col+1;
+        if(col+1 == 9){
+            nextrow++;
+            nextcol = 0;
+        }
+        if(mat[row][col] != 0){
+            return solve(mat,nextrow,nextcol);
+        }
         
-        bool res = false;
         
-        for(int i = 1; i <= 9; i++) {
-            int x = to_fill[index].first, y = to_fill[index].second;
-            
-            if(valid(mat, i, x, y)) {
-                mat[x][y] = i;
-                res |= solve(mat, to_fill, index + 1);
-                if(res) return true;
-                mat[x][y] = 0;
+        for(int i = 1; i<=9; i++){
+            if(issafe(mat,row,col,i)){
+                mat[row][col] = i;
+                if(solve(mat,nextrow,nextcol)){
+                    return true;
+                }
+                mat[row][col] = 0;
             }
         }
         
-        return res;
+        return false;
+        
     }
+
     void solveSudoku(vector<vector<int>> &mat) {
-        vector<pair<int, int>> to_fill;
-        int n = mat.size();
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 0) to_fill.push_back({i, j});
-            }
-        }
+        solve(mat,0,0);
         
-        solve(mat, to_fill, 0);
     }
 };
-
-//{ Driver Code Starts.
-
-vector<int> inputLine() {
-    string str;
-    getline(cin, str);
-    stringstream ss(str);
-    int num;
-    vector<int> res;
-    while (ss >> num) {
-        res.push_back(num);
-    }
-    return res;
-}
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<vector<int>> grid;
-        for (int i = 0; i < 9; i++) {
-            vector<int> v = inputLine();
-            grid.push_back(v);
-        }
-
-        Solution ob;
-
-        ob.solveSudoku(grid);
-
-        for (auto v : grid) {
-            for (auto elem : v) {
-                cout << elem << " ";
-            }
-            cout << endl;
-        }
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
