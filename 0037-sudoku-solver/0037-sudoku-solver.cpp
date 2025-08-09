b01663a7d5ec33db30f8180bb60f29a1969292a6
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-10 00:11:26 +0530

    Time: 310 ms (66.47%), Space: 8.7 MB (87.69%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,45 +1,69 @@
 class Solution {
 public:
-    bool issafe(int row, int col, vector<vector<char>>& board, char val) {
-        int n = board.size();
-        for (int i = 0; i < n; i++) {
-            if (board[row][i] == val) {
+    bool issafe(vector<vector<char>>&board, int row, int col,char digit){
+        //downward
+        for(int i = 0; i <= 8; i++){
+            if(board[i][col] == (digit)){
                 return false;
             }
-            if (board[i][col] == val) {
+        }
+
+        //horizontal
+        for(int i = 0; i <= 8; i++){
+            if(board[row][i] == (digit)){
                 return false;
             }
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == val) {
-                return false;
+        }
+
+        //grid
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+
+        for(int i = sr; i<sr+3; i++){
+            for(int j = sc ; j <sc+3; j++){
+                if(board[i][j] == (digit)){
+                    return false;
+                }
             }
+        }
+
+        return true;
+
+    }
+
+    bool solve(vector<vector<char>>&board, int row, int col){
+
+        if(row == 9){
+            return true;
+        }
+        int nextrow = row, nextcol = col+1;
+        if(col+1 == 9){
+            nextrow++;
+            nextcol = 0;
+        }
+
+        
+            
+       
+        if(board[row][col] == '.'){
+            for(char digit = '1'; digit <= '9'; digit++){
+                if(issafe(board, row,col,digit)){
+                    board[row][col] = (digit);
+                    if(solve(board,nextrow,nextcol)){
+                        return true;
+                    }
+                    board[row][col] = '.';
+                }
+            }
+
+            return false;
+        } else{
+            return solve(board, nextrow,nextcol);
         }
         return true;
     }
 
-    bool solve(vector<vector<char>>& board) {
-        int n = board.size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '.') {  
-                    for (char val = '1'; val <= '9'; val++) {
-                        if (issafe(i, j, board, val)) {
-                            board[i][j] = val;
-                            if (solve(board)) {
-                                return true;
-                            } else {
-                                board[i][j] = '.'; 
-                            }
-                        }
-                    }
-                    return false; 
-                }
-            }
-        }
-        return true;  
-    }
-
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        solve(board,0,0);
     }
 };
