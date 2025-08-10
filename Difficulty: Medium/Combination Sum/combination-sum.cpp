c3ff4d7c4260019965a9cefc5200eebf6b023a5b
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-11 02:00:52 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Combination Sum/combination-sum.cpp
@@ -0,0 +1,33 @@
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find all combinations of elements
+    // in array arr that sum to target.
+    vector<vector<int>>ans;
+    
+    void solve(vector<int>&arr, int indx, int sum, vector<int>temp,int target){
+        if(sum == target){
+            ans.push_back(temp);
+            return;
+        }
+        
+        if(indx >= arr.size() || sum > target) return;
+        
+        temp.push_back(arr[indx]);
+        solve(arr, indx, sum + arr[indx],temp,target);
+        temp.pop_back();
+    
+        solve(arr,indx+1,sum,temp,target);
+    }
+    
+    
+    vector<vector<int>> combinationSum(vector<int> &arr, int target) {
+        // Your code here
+        vector<int>temp;
+        
+        solve(arr,0,0,temp,target);
+        
+        return ans;
+    }
+};
