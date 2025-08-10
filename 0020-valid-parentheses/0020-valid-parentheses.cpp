3ac0520761fab5fab5b88e7703cc17e8d4371928
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-10 16:20:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
+       
         stack<char>st;
-
-        for(int i = 0 ; i < s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
-                st.push(s[i]);
-            
+        for(int i = 0; i < s.length(); i++){
+            char ch = s[i];
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
             else{
-                if(!st.empty()){
-                    char ch = st.top();
-
-                    if((s[i] == ')' && ch == '(') || (s[i] == '}' && ch == '{') || (s[i] == ']' && ch == '[')){
-                        st.pop();
-                    }
-                    else{
-                        return false;
-                    }
+                char check = st.top();
+                if((ch == ')' && check == '(' )||(ch == '}' && check == '{') || (ch == ']' && check == '[')){
+                    st.pop();
                 }
                 else{
                     return false;
@@ -24,12 +19,10 @@ public:
             }
         }
 
-        if(st.empty())
+        if(st.empty()){
             return true;
-        
-        else
-            return false;
-        
+        }
         return false;
+       
     }
 };
