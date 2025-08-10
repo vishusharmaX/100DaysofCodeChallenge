3b7caf01c6ab845b175b5356ef1063c269a58adf
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-10 15:49:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
@@ -1,68 +1,21 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution
-{
-    public:
-    //Function to delete middle element of a stack.
-    void solve(stack<int>&s, int k){
-         if(k == 1){
+class Solution {
+  public:
+    // Function to delete middle element of a stack.
+    void solve(stack<int>&s, int cnt, int size){
+        if(cnt  == size/2){
             s.pop();
-            return ;
+            return;
         }
         
-        int temp = s.top();
+        int ans = s.top();
         s.pop();
-        solve(s,k-1);
-        s.push(temp);
+        solve(s,cnt+1,size);
+        s.push(ans);
     }
     
-    void deleteMid(stack<int>&s, int sizeOfStack)
-    {
-        // code here.. 
-        if(sizeOfStack == 0)
-            return ;
-        
-        int k = (sizeOfStack/2 )+1;
-        solve(s,k);
+    void deleteMid(stack<int>& s) {
+        int cnt = 0;
+        int n = s.size();
+        solve(s,cnt,n);
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin>>t;
-    
-    while(t--)
-    {
-        int sizeOfStack;
-        cin>>sizeOfStack;
-        
-        stack<int> myStack;
-        
-        for(int i=0;i<sizeOfStack;i++)
-        {
-            int x;
-            cin>>x;
-            myStack.push(x);    
-        }
-
-            Solution ob;
-            ob.deleteMid(myStack,myStack.size());
-            while(!myStack.empty())
-            {
-                cout<<myStack.top()<<" ";
-                myStack.pop();
-            }
-        cout<<endl;
-    }   
-    return 0;
-}
-
-// } Driver Code Ends
