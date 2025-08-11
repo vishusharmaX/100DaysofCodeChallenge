c2013170c82e336831ae135e4d10d14e4f9eac74
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-11 20:07:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
@@ -1,63 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
 
     // Function to return length of longest subsequence of consecutive integers.
     int longestConsecutive(vector<int>& arr) {
-         int N = arr.size();
-         if(N==0) return 0;
-         
-        int longest = 1;
-        unordered_set<int> st;
-        for(int i =0  ; i < N ; i++){
-            st.insert(arr[i]);
-        }
-
-        for(auto it : st){
-            if(st.find(it-1) == st.end()){
-                int cnt = 1;
-                int x = it;
-                while(st.find(x+1) != st.end()){
-                    x = x + 1 ;
-                    cnt = cnt + 1 ;
-                }
-                longest = max(longest ,cnt);
+        // Your code here
+        sort(arr.begin(),arr.end());
+        int cnt = 1;
+        int maxlen = 0;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] == arr[i-1]){
+                continue;
             }
+            else if(arr[i] == arr[i-1]+ 1){
+                cnt++;
+            }else{
+                
+                maxlen = max(maxlen,cnt);
+                cnt = 1;
+            }
+            
         }
-
-        return longest;
+        maxlen = max(maxlen, cnt); // check at the end
+        return maxlen;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.longestConsecutive(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
