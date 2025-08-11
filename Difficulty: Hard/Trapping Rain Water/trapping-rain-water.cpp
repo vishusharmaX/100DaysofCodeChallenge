b7f6ee97fa567aebb903a758fac9d6a8288f15e2
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-11 20:31:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -1,58 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int maxWater(vector<int> &a) {
-        vector<int>l=a;
-        vector<int>r=a;
+    vector<int>solve(vector<int>arr,int n){
+        vector<int>ans(n,-1);
+        ans[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            ans[i] = max(arr[i],ans[i-1]);
+        }
         
-        for(int i=1;i<a.size();i++){
-            l[i]=max(l[i],l[i-1]);
-        }
-        for(int i=a.size()-1;i>=1;i--){
-            r[i-1]=max(r[i],r[i-1]);
-        }
-        int sum=0;
-        for(int i=1;i<a.size()-1;i++){
-            sum+=max(0,min(l[i],r[i])-a[i]);
-        }
-        return sum;
+        return ans;
         
+        
+    }
+    vector<int>solve1(vector<int>arr,int n){
+        vector<int>ans(n,-1);
+        ans[n-1] = arr[n-1];
+        for(int i = n-2; i>= 0; i--){
+            ans[i] = max(arr[i],ans[i+1]);
+        }
+        return ans;
+        
+    }
+  
+    int maxWater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int>leftmax = solve(arr,n);
+        vector<int>rightmax = solve1(arr,n);
+        int total = 0;
+        for(int i =0; i < arr.size(); i++){
+            int h = min(leftmax[i],rightmax[i]);
+            int w = arr[i];
+            
+            total = total + (h-w);
+        }
+        
+        return total;
     }
 };
-
-
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
