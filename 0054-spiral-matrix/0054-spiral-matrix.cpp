e53bda7b4051960fe68611c223eca3c205c1981a
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-12 14:21:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (97.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int startrow = 0;
-        int startcol = 0;   
-        int endingrow = n-1;
-        int endingcol = m-1;
-        int cnt = 0;
-        int total = n *m;
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int m = mat.size() , n = mat[0].size();
+        int sr = 0 , sc = 0 , er = m-1, ec = n-1;
         vector<int>ans;
-        while(cnt <total){
+        while(sr <= er  && sc <= ec){
+            //top
+            for(int i = sc ; i <= ec ; i++){
+                ans.push_back(mat[sr][i]);
+            }
+            //right
+            for(int i = sr+1 ; i <=er ; i++){
+                ans.push_back(mat[i][ec]);
+            }
+            //bottom
+            for(int i = ec-1 ; i >=sc ; i--){
+                if(sr == er){
+                    break;
+                }
+                ans.push_back(mat[er][i]);
+            }
+            //left
+            for(int i = er-1 ; i >=sr+1 ; i--){
+                if(ec == sc){
+                    break;
+                }
+                ans.push_back(mat[i][sc]);
+            }
+            sr++;
+            ec--;
+            er--;
+            sc++;
 
-            for(int i = startcol ; i <=endingcol && cnt < total ; i++){
-                ans.push_back(matrix[startrow][i]);
-                cnt++;
-            }
-            startrow++;
-
-            for(int i = startrow ; i <=endingrow  && cnt < total; i++){
-                ans.push_back(matrix[i][endingcol]);
-                cnt++;
-            }
-            endingcol--;
-            for(int i = endingcol ; i >= startcol && cnt < total ; i--){
-                ans.push_back(matrix[endingrow][i]);
-                cnt++;
-            }
-            endingrow--;
-            for(int i = endingrow ; i >= startrow && cnt < total ; i--){
-                ans.push_back(matrix[i][startcol]);
-                cnt++;
-            }
-            startcol++;
         }
+
         return ans;
     }
 };
