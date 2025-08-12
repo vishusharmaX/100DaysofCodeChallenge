8c20ee3a68d7a079fbaf6653678d8bc0fceaa011
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-13 00:50:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
+++ Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
@@ -1,63 +1,33 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
 
-// } Driver Code Ends
-
-
-class Solution
-{
-    public:
-    //Function to find the largest number after k swaps.
-    void solve(string &s, string &res, int start, int k){
-        
-        if(k == 0 || start>=s.size()-1)
+class Solution {
+  public:
+    // Function to find the largest number after k swaps.
+    void solve(string &s, int k , int start, string &res){
+        if(k == 0 || start>= s.length()-1){
             return;
-        char ch = *max_element(s.begin()+start,s.end());
-        for(int i=start; i<s.size() ; i++){
-            
-            if(s[i] > s[start] && s[start] < ch){
-                swap(s[start],s[i]);
-                
-                if(s.compare(res) > 0){
-                    res = s;
-                }
-                
-                solve(s,res,start+1,k-1);
-                swap(s[start],s[i]);
-            }
-            
         }
         
-        solve(s,res,start+1,k);
+        for(int i = start+1; i< s.length(); i++){
+            char maxi = *max_element(s.begin()+start+1,s.end());
+            if(s[start] < s[i] && s[i] == maxi){
+                swap(s[start],s[i]);
+                if(s.compare(res)>0){
+                    res = s;
+                }
+                solve(s,k-1,start+1,res);
+                swap(s[start],s[i]);
+            }
+        }
+        
+        solve(s,k,start+1,res);
+        
     }
     
-    string findMaximumNum(string str, int k)
-    {
-       // code here.
-       string res = str;
-       solve(str,res,0,k);
-       
-       return res;
+    string findMaximumNum(string& s, int k) {
+        // code here.
+        string res = s;
+        solve(s,k,0,res);
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t, k;
-    string str;
-
-    cin >> t;
-    while (t--)
-    {
-        cin >> k >> str;
-        Solution ob;
-        cout<< ob.findMaximumNum(str, k) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
