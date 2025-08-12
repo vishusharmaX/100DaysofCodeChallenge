b02906476cb01c91849b7c88790c2b97a7869fbe
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-12 14:33:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -1,79 +1,47 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
         // code here
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int left = 0 , right = m-1;
-        int top = 0, bottom = n-1;
-        
+        int n = mat.size(), m = mat[0].size();
+        int sr = 0 , sc = 0 , er= n-1, ec = m-1;
         vector<int>ans;
-        
-        while(left <= right && top <= bottom){
-           
-           for(int i = left; i <=right; i++){
-               ans.push_back(matrix[top][i]);
-           }
-           top++;
-           for(int i = top; i <= bottom ; i++){
-               ans.push_back(matrix[i][right]);
-           }
-           right--;
-           
-           if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    ans.push_back(matrix[bottom][i]);
-                } 
-                bottom--;
+        while(sr <= er && sc <= ec){
+            
+            //top
+            for(int i = sc; i<= ec; i++){
+                ans.push_back(mat[sr][i]);
             }
             
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.push_back(matrix[i][left]);
-                }
-                left++;
+            
+            //right
+            for(int i = sr+1; i<= er; i++){
+                ans.push_back(mat[i][ec]);
             }
-           
-           
+            
+            
+            //bottom
+            for(int i = ec-1; i >= sc; i--){
+                if(sr == er){
+                    break;
+                }
+                ans.push_back(mat[er][i]);
+            }
+            
+            //left
+            for(int i = er-1; i>= sr+1; i--){
+                if(ec == sc){
+                    break;
+                }
+                ans.push_back(mat[i][sc]);
+            }
+            
+            sr++;
+            ec--;
+            er--;
+            sc++;
         }
         
         return ans;
         
-        
-        
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int r, c;
-        cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                cin >> matrix[i][j];
-            }
-        }
-
-        Solution ob;
-        vector<int> result = ob.spirallyTraverse(matrix);
-        for (int i = 0; i < result.size(); ++i)
-            cout << result[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
