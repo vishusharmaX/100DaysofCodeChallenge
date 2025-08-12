069c52df08b7afd4eaf84be5d1a91be0116d729d
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-13 01:18:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/N Digit numbers with digits in increasing order/n-digit-numbers-with-digits-in-increasing-order.cpp
+++ Difficulty: Medium/N Digit numbers with digits in increasing order/n-digit-numbers-with-digits-in-increasing-order.cpp
@@ -1,64 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
-    void solve(int n,vector<int>&v,vector<int>&res){
+    
+    void solve(int n, vector<int>&v, vector<int>&ans){
         
         if(n == 0){
-            int ans = 0;
+            int res=0;
             for(int i = 0; i < v.size(); i++){
-                ans = ans*10 + v[i];
+                res = res*10 + v[i];
             }
-            res.push_back(ans);
-            return;
+            ans.push_back(res);
         }
         
-        for(int i = 1; i <= 9 ; i++){
+        for(int i = 1; i <=9; i++){
             if(v.size() == 0 || i > v[v.size()-1]){
                 v.push_back(i);
-                solve(n-1,v,res);
+                solve(n-1,v,ans);
                 v.pop_back();
             }
-        }
-                
+        } 
         
     }
+  
     vector<int> increasingNumbers(int n) {
         // Write Your Code here
-        vector<int>res;
+        vector<int>ans;
         if(n == 1){
-            for(int i = 0 ; i <= 9; i++){
-                res.push_back(i);
+            for(int i  = 0; i<=9; i++){
+                ans.push_back(i);
             }
             
-            return res;
+            return ans;
         }
         vector<int>v;
-        solve(n,v,res);
-        
-        return res;
+        solve(n,v,ans);
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        Solution ob;
-        vector<int> ans = ob.increasingNumbers(N);
-        for (auto num : ans) {
-            cout << num << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
