c08c0903fb4763dce5f359fb872f301f61677296
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-12 16:46:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -1,22 +1,29 @@
 class Solution {
   public:
     // Function to find a solved Sudoku.
-    bool issafe(vector<vector<int>>&mat,int row, int col, int digit){
+    
+    
+    bool issafe(vector<vector<int>>&mat,int row , int col, int digit){
         
         
-        for(int  i = 0; i <=8; i++){
-            if(mat[i][col] == digit) return false;
+        for(int i = 0; i < mat.size(); i++){
+              if(mat[i][col] == digit){
+                  return false;
+              } 
         }
-        for(int  i = 0; i <=8; i++){
-            if(mat[row][i] == digit) return false;
+        
+        for(int j = 0; j < mat[0].size(); j++){
+            if(mat[row][j] == digit){
+                return false;
+            }
         }
         
-        int sr = (row / 3) * 3;
-        int sc = (col / 3) * 3;
-
         
-        for(int i = sr; i < sr +3;i++){
-            for(int j =sc ; j < sc +3;j++ ){
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+        
+        for(int i = sr ; i < sr+3; i++){
+            for(int j = sc;j <sc +3; j++ ){
                 if(mat[i][j] == digit){
                     return false;
                 }
@@ -25,23 +32,28 @@ class Solution {
         
         
         return true;
+        
+        
     }
-
-    bool solve(vector<vector<int>>&mat, int row, int col){
+    
+    
+    
+    bool solve(vector<vector<int>>&mat, int row,int col){
+        
         if(row == 9){
             return true;
         }
+        
         int nextrow = row, nextcol = col+1;
-        if(col+1 == 9){
+        if(col +1  == 9){
             nextrow++;
             nextcol = 0;
         }
-        if(mat[row][col] != 0){
+         if(mat[row][col] != 0){
             return solve(mat,nextrow,nextcol);
         }
         
-        
-        for(int i = 1; i<=9; i++){
+        for(int i = 1; i<= 9; i++){
             if(issafe(mat,row,col,i)){
                 mat[row][col] = i;
                 if(solve(mat,nextrow,nextcol)){
@@ -51,11 +63,12 @@ class Solution {
             }
         }
         
-        return false;
+         return false;
         
     }
 
     void solveSudoku(vector<vector<int>> &mat) {
+        
         
         solve(mat,0,0);
         
