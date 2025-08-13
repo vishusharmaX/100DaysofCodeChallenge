4a896bd374a051f34a7484e528f6ab92c3f67f20
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-14 00:03:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.cpp
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -1,6 +1,27 @@
 class Solution {
   public:
-    void nextPermutation(vector<int>& arr) {
-        next_permutation(arr.begin(),arr.end());
+    void nextPermutation(vector<int>& nums) {
+        int indx = -1;
+        int n = nums.size();
+        for(int i = n-2; i>= 0; i--){
+            if(nums[i] < nums[i+1]){
+                indx = i;
+                break;
+            }
+        }
+        
+        if(indx == -1){
+            sort(nums.begin(),nums.end());
+        }else{
+            for(int i = n-1; i>=0;i--){
+                if(nums[i] > nums[indx]){
+                    swap(nums[i],nums[indx]);
+                    break;
+                }
+            }
+            sort(nums.begin()+indx+1,nums.end());
+        }
+        
+        
     }
 };
