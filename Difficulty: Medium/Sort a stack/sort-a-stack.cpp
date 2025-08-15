65410429169b4bb265986b7cd299018562a41e32
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-15 12:47:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -9,31 +9,26 @@ public:
 /* The below method sorts the stack s
 you are required to complete the below method */
 
-void sortkarkelao(stack<int>&st, int num){
-    if(st.empty() || (!st.empty() && st.top() < num)){
-        st.push(num);
+void solve(stack<int>&s,int x){
+    
+    if(s.size() == 0  || !s.empty() && s.top() < x){
+        s.push(x);
         return;
     }
     
-    int ans = st.top();
-    st.pop();
-    sortkarkelao(st,num);
-    st.push(ans);
+    int ans = s.top();
+    s.pop();
+    solve(s,x);
+    s.push(ans);
+    
 }
-
 
 void SortedStack ::sort() {
     // Your code here
+    if(s.empty())return;
     
-    if(s.empty()){
-        return;
-    }
-    
-    int num = s.top();
+    int ele = s.top();
     s.pop();
-     sort();
-    sortkarkelao(s,num);
-    
-    
-    
+    sort();
+    solve(s,ele);
 }
