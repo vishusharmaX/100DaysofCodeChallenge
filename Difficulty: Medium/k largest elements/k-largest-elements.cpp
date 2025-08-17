643f5ba5045d6965d5b0f7ba03a8084c0eb7b79b
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-17 16:24:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k largest elements/k-largest-elements.cpp
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -1,58 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> kLargest(vector<int>& arr, int k) {
         // Your code here
-        vector<int> ans;
-        priority_queue<int,vector<int>,greater<int>> minh;
-        for(int i=0;i<arr.size();i++){
-            minh.push(arr[i]);
-            if(minh.size()>k){
-                minh.pop();
-            }
+        priority_queue<int>pq;
+        
+        for(int i = 0; i < arr.size(); i++){
+            pq.push(arr[i]);
+        }
+        vector<int>ans;
+        while(k--){
+            ans.push_back(pq.top());
+            pq.pop();
         }
         
-        while(minh.size()>0){
-            ans.push_back(minh.top());
-            minh.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.kLargest(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
