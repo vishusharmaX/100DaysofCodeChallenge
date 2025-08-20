23fd520345f7a4073927d3a0899cfe80c8aa670d
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-20 16:26:58 +0530

    Time: 3 ms (2%), Space: 11.1 MB (100%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-    int findPivot(vector<int>&arr){
-        int n = arr.size();
-        int s = 0 ;
-        int e = n - 1;
-        int mid = s + (e-s)/2;
-        while(s < e){
-            if(arr[mid] >= arr[0]){
-                s = mid+1;
+    int search(vector<int>& nums, int target) {
+     int n = nums.size();
+      int left = 0;
+      int right = n-1;
+      int mid= left + (right - left) / 2;
+      while(left <= right){
+        if(nums[mid] == target)
+         return mid;
+        if(nums[mid] >= nums[left]) {
+          if(target >= nums[left] && target <= nums[mid])
+            {
+                right = mid - 1;
             }
-            else{
-                e = mid;
-            }
-            mid = s + (e-s)/2;
+           else left = mid + 1;
+        } 
+        else {
+          if(target >= nums[mid] && target <= nums[right]) 
+            left = mid + 1;
+          else right = mid - 1;
         }
-
-        return s;
-    }
-    int solve(int s , int e , vector<int>arr, int target){
-        int mid = s +(e-s)/2;
-        while(s <= e){
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if(arr[mid] > target){
-                e = mid - 1;
-            }
-            else{
-                s = mid +1;
-            }
-            mid = s + (e-s)/2;
-        }
-
-        return -1;
-    }
-
-    int search(vector<int>& arr, int target) {
-        int pivot = findPivot(arr);
-        int n = arr.size();
-        int ans = -1;
-        if(target >= arr[pivot] && target <= arr[n-1]){
-            ans = solve(pivot,n-1,arr,target);
-        }
-        else{
-            ans = solve(0,pivot-1,arr,target);
-        }
-
-        return ans;
+          mid = left + (right - left) / 2;
+      }
+      return -1;
     }
 };
