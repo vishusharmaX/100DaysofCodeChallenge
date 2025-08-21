767e5a61522723a59a34ad69334484de1965b502
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-22 01:13:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,72 +1,36 @@
-//{ Driver Code Starts
-// driver code
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution
-{
+class Solution {
   public:
-    long long minTime(int arr[], int n, int k)
-    {
+    int minTime(vector<int>& arr, int k) {
         // code here
-        // return minimum time
-        long long start = 0 ;
-        long long  end =  0;
-        long long ans = 0;
-        
-        for(int i = 0 ; i <n; i++){
-           if(start < arr[i])
-                start = arr[i];
+        int n = arr.size();
+        if(k > n) return -1;
+        int start = 0 , end = 0;
+        for(int i = 0; i< arr.size(); i++){
+            start = max(start,arr[i]);
             end += arr[i];
         }
-        
+        int ans = 0;
         while(start <= end){
-            long long mid = start +  (end-start)/2;
-            long long cnt = 1 ,time = 0;
-            for(int i = 0 ; i< n ;i++){
-                time+=arr[i];
-                
-                if(time > mid){
+            int mid = start + (end-start)/2;
+            int painter = 0 , cnt = 1;
+            
+            for(int i = 0; i < n; i++){
+                painter  += arr[i];
+                if(painter  > mid){
                     cnt++;
-                    time = arr[i];
+                    painter  = arr[i];
                 }
             }
             
             if(cnt <= k){
-                ans = mid ;
-                end= mid-1;
+                ans = mid;
+                end = mid -1;
             }
-            else
+            else{
                 start = mid +1;
+            }
         }
         
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int k,n;
-		cin>>k>>n;
-		
-		int arr[n];
-		for(int i=0;i<n;i++)
-		    cin>>arr[i];
-		Solution obj;
-		cout << obj.minTime(arr, n, k) << endl;
-	}
-	return 0;
-}
-// } Driver Code Ends
