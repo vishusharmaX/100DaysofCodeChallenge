925275686394ba5ca3eea7268ca33af17ce8b55b
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-24 23:18:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void immediateSmaller(vector<int>& arr) {
+        int n = arr.size();
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                arr[i] = arr[i + 1];
+            } else {
+                arr[i] = -1;
+            }
+        }
+        
+        arr[n - 1] = -1;
+    }
+};
