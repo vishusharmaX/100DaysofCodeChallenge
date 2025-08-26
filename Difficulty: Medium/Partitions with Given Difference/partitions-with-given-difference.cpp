e4ab25d84a48e4b0d46b929141725463631740c4
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-26 20:20:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,33 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int total_sum = 0;
-        for(auto it : arr){
-            total_sum+=it;
-        }
+    int solve(vector<int>&arr, int sum , vector<vector<int>>&dp){
         
-        
-        
-        int sum = (d+total_sum)/2;
-        
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
-        if ((total_sum + d) % 2 != 0) return 0;
-        for(int i = 0; i < n+1; i++){
-            dp[i][0] = 1;
-        }
-        const int mod = 1000000007;
+        int n = arr.size();
         for(int i = 1; i < n+1; i++){
-            for(int j= 0; j<sum+1; j++){
+            for(int j = 0; j < sum+1; j++){
                 if(arr[i-1] <= j){
-                    dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j])%mod;
-                }
-                else{
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+                }else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
@@ -36,26 +16,22 @@ class Solution {
         return dp[n][sum];
         
     }
-};
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, d;
-        cin >> n >> d;
-        vector<int> arr;
-
-        for (int i = 0; i < n; ++i) {
-            int x;
-            cin >> x;
-            arr.push_back(x);
+  
+    int countPartitions(vector<int>& arr, int d) {
+        int total = 0;
+        for (int num : arr) {
+            total += num;
         }
 
-        Solution obj;
-        cout << obj.countPartitions(n, d, arr) << "\n";
+        if (total < d || (total - d) % 2 != 0) return 0;
+
+        int target = (total - d) / 2;
+        int n = arr.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        return solve(arr, target, dp);
     }
-    return 0;
-}
-// } Driver Code Ends
+};
