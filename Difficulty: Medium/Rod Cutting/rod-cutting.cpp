ac5cc7e86f89d61b6e6b1cdf3d53f2defca6a9a6
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-26 23:42:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -1,26 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
-class Solution{
+class Solution {
   public:
-    int cutRod(int price[], int n) {
-        //code here
-       
+    int cutRod(vector<int> &price) {
+        // code here
+        int n = price.size();
+        vector<int>length;
+        for(int i = 0;i < n; i++){
+            length.push_back(i+1);
+        }
+        
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < n+1; j++){
-                if(i <= j){
-                    dp[i][j] = max(price[i-1]+dp[i][j-i] , dp[i-1][j]);
-                }
-                else{
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1;j++){
+                if(length[i-1] <= j){
+                    dp[i][j] = max(dp[i-1][j] , price[i-1] + dp[i][j-length[i-1]]);
+                }else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
@@ -29,25 +25,3 @@ class Solution{
         return dp[n][n];
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++) 
-            cin >> a[i];
-            
-        Solution ob;
-
-        cout << ob.cutRod(a, n) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
