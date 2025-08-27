bfcdca0e22843dddda36c1b3cf88110758270660
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-28 01:32:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,60 +1,22 @@
-//{ Driver Code Starts
-//Initial template for C++
+// User function template for C++
 
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function template for C++
-
-
-class Solution
-{
-    public:
-    //Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string s, string t, int n, int m)
-    {
-        // int n = s.length();
-        // int m = t.length();
+class Solution {
+  public:
+    // Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string &str1, string &str2) {
+        // code here
+        int n = str1.length(); int m = str2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        for(int i = 1 ; i < n+1; i++){
-            for(int j = 1; j < m+1; j++){
-                if(s[i-1] == t[j-1]){
+        for(int i = 1;i < n+1; i++){
+            for(int j = 1;j < m+1; j++){
+                if(str1[i-1] == str2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
-
-        return (n+m-dp[n][m]);
+        return (n+m - dp[n][m]);
     }
 };
-
-//{ Driver Code Starts.
-
-int main()
-{   
-    
-    int t;
-    
-    //taking total testcases
-    cin >> t;
-    while(t--){
-    string X, Y;
-    //taking String X and Y
-	cin >> X >> Y;
-	
-	//calling function shortestCommonSupersequence()
-	Solution obj;
-	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
-    }
-	return 0;
-}
-
-
-// } Driver Code Ends
