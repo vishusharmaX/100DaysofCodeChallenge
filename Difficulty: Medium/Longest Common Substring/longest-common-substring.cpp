9e7b7507bc33ca51d97364ce3c4d9ece7a4d14a7
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-27 19:55:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,52 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int longestCommonSubstr(string s, string t) {
+    int longestCommonSubstr(string& s1, string& s2) {
         // your code here
-        int n = s.length(), m = t.length();
-        vector<vector<int>>dp(n+1, vector<int>(m+1,0));
-        
+        int n = s1.length() , m = s2.length();
+        vector<vector<int>>t(n+1,vector<int>(m+1,0));
         
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m+1; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 0;
+                if(s1[i-1] == s2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }else{
+                    t[i][j] = 0;
                 }
             }
         }
         
         int maxi = INT_MIN;
-         for(int i = 0; i < n+1; i++){
-            for(int j = 0; j < m+1; j++){
-               if(dp[i][j] > maxi){
-                   maxi= dp[i][j];
-               }
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                maxi = max(maxi,t[i][j]);
             }
         }
         
         return maxi;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s1, s2;
-        cin >> s1 >> s2;
-        Solution ob;
-
-        cout << ob.longestCommonSubstr(s1, s2) << endl;
-    }
-}
-// } Driver Code Ends
