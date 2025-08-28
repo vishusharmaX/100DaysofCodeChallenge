599d54fa24a0fc9a43f49fd55619cc654b671e89
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-29 00:48:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
+++ Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -1,69 +1,45 @@
-//{ Driver Code Starts
-// Initial Template for c++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    bool isPalindrome(string s,int i, int j){
+class Solution {
+  public:
+    bool isPalli(string s, int i , int j){
+        
         while(i < j){
             if(s[i] != s[j]){
                 return false;
             }
-            else{
-                i++;
-                j--;
-            }
+            i++;
+            j--;
         }
+        
         return true;
     }
-    int solve(string s, int i , int j,vector<vector<int>>&dp){
-        if(i>=j || isPalindrome(s, i, j) ){
-            return 0;
-        }
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
+  
+    int solve(string s, int i , int j, vector<vector<int>>&dp){
         
-        int mini = INT_MAX;
-        for(int k = i ; k<=j-1; k++){
-              if (isPalindrome(s, i, k)) {
-                  
-            int tempans = 1 + solve(s,i,k,dp)+ solve(s,k+1,j,dp);
-            mini = min(mini,tempans);
+        if(i >= j)return 0;
+        
+        if(isPalli(s,i,j))return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int ans = INT_MAX;
+        for(int k = i ; k <= j-1;k++){
+           if (isPalli(s, i, k)) {
+                  int tempans = 1 + solve(s,i,k,dp)+ solve(s,k+1,j,dp);
+                  ans = min(ans,tempans);
               }
         }
         
-        return dp[i][j] = mini;
+        return dp[i][j] = ans;
+        
+        
     }
-    int palindromicPartition(string s)
-    {
+  
+    int palPartition(string &s) {
         // code here
         int n = s.length();
-        int i = 0;
-        int j = s.length()-1;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return  solve(s,i,j,dp);
+         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(s,0,s.length()-1,dp);
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        string str;
-        cin>>str;
-        
-        Solution ob;
-        cout<<ob.palindromicPartition(str)<<"\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
