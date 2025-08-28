4fec8eea13215de878a228cf061106f6cd1f4232
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-28 17:05:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -1,46 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution{
+class Solution {
   public:
-    int minimumNumberOfDeletions(string s) { 
+    int minDeletions(string s) {
         // code here
         string t = s;
         reverse(t.begin(),t.end());
-        int n = s.length() , m = t.length();
-        
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        
-        for(int i = 1; i  < n+1; i++){
-            for(int j = 1; j < m+1; j++){
+        int n = s.length();
+        // cout<<n<<endl;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1; j++){
                 if(s[i-1] == t[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
+                }else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        // cout<<dp[n][n]<<endl;
         
-        return n-dp[n][m];
-    } 
-};
-
-//{ Driver Code Starts.
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        string S;
-        cin >> S;
-        Solution obj;
-        cout << obj.minimumNumberOfDeletions(S) << endl;
+        return (n - dp[n][n]);
+        
     }
-    return 0;
-}
-// } Driver Code Ends
+};
