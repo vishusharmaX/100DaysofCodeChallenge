728d8adfb526cf20aadced9afd38718dd7833db4
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-28 16:49:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -1,52 +1,25 @@
-//{ Driver Code Starts
-//Initial template for C++
+// User function Template for C++
 
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
+class Solution {
   public:
-    int longestPalinSubseq(string s) {
-        //code here
-        string s1 = s;
-        reverse(s1.begin(),s1.end());
-        string t = s1;
+    int longestPalinSubseq(string &s) {
+        // code here
+        string t =s; 
+        reverse(t.begin(),t.end());
+
         int n = s.length();
-        int m = t.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        
-        for(int i = 1;i <n+1; i++){
-            for(int j = 1;j < m+1; j++){
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1;j++){
                 if(s[i-1] == t[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
+                }else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        
-        return dp[n][m];
-        
+
+        return dp[n][n];
     }
 };
-
-//{ Driver Code Starts.
-
-int32_t main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
-        Solution ob;
-        cout << ob.longestPalinSubseq(s) << endl;
-    }
-}
-// Contributed By: Pranay Bansal
-
-// } Driver Code Ends
