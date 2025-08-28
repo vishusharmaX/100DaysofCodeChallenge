ee9842e209d78d61d13634262af1a15db351af0d
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-28 16:21:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -1,57 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
 
-// } Driver Code Ends
-class Solution{
-		
-
-	public:
-	int minOperations(string s, string t) 
-	{ 
-	   int n = s.length() , m = t.length();
-	   
-	   vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-	   
-	   for(int i = 1; i < n+1; i++){
-	       for(int j = 1; j < m+1;j++){
-	           if(s[i-1] == t[j-1]){
-	               dp[i][j] = 1 + dp[i-1][j-1];	           
-	               
-	           }
-	           else{
-	               dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-	           }
-	       }
-	   }
-	   
-	   int del = n -dp[n][m];
-	   int insert = m -dp[n][m];
-	   
-	   return del + insert;
-	   
-	    
-	} 
-};
-
-//{ Driver Code Starts.
-int main() 
-{
-   	
-   
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-        string s1, s2;
-        cin >> s1 >> s2;
-
-	    Solution ob;
-	    cout << ob.minOperations(s1, s2) << "\n";
-	     
+  public:
+    int minOperations(string &s1, string &s2) {
+        // Your code goes here
+        int n = s1.length() , m = s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        int ans = n + m - 2*dp[n][m];
+        
+        return ans;
     }
-    return 0;
-}
-
-
-// } Driver Code Ends
+};
