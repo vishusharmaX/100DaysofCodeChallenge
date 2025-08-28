1ce41803ed99dfa37baaac2ae4f483ad30269a32
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-08-28 22:57:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,56 +1,34 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution{
-public:
-    int solve(int arr[], int i , int j,vector<vector<int>>&dp){
-        //base condtions
-        if(i >= j)
-            return 0;
+class Solution {
+  public:
+    
+    int solve(vector<int>&arr, int i , int j, vector<vector<int>>&dp){
         
-        if(dp[i][j] != -1)
+        if(i >= j)return 0;
+        
+        
+        if(dp[i][j] != -1){
             return dp[i][j];
-        
+        } 
+         
+         
         int mini = INT_MAX;
-        for(int k = i ; k <= j-1; k++){
-            int tempans = solve(arr,i,k,dp) + arr[i-1]*arr[k]*arr[j] + solve(arr,k+1,j,dp);
+        
+        for(int k = i; k <= j-1; k++){
+            int tempans = solve(arr, i, k,dp) + solve(arr,k+1,j,dp)+arr[i-1]*arr[k]*arr[j];
             mini = min(tempans,mini);
         }
         
-        return dp[i][j] = mini;
         
+        return dp[i][j] = mini;
     }
     
-    int matrixMultiplication(int N, int arr[])
-    {
+  
+    int matrixMultiplication(vector<int> &arr) {
         // code here
-        vector<vector<int>>dp(N+1,vector<int>(N+1,-1));
-        return solve(arr,1,N-1,dp);
+        int n = arr.size();
+       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(arr,1,n-1,dp);
+        
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        int arr[N];
-        for(int i = 0;i < N;i++)
-            cin>>arr[i];
-        
-        Solution ob;
-        cout<<ob.matrixMultiplication(N, arr)<<endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
