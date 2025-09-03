47420e77b34490040324e11cf706f8721ba18f90
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-09-03 18:26:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
@@ -7,15 +7,17 @@ class Solution {
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
         for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < n+1;j++){
-                if(s[i-1] == t[j-1] && i !=j){
+            for(int j = 1; j < n+1; j++){
+                if(s[i-1] == t[j-1] && i != j){
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
+                }
+                else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         
         return dp[n][n];
+        
     }
 };
