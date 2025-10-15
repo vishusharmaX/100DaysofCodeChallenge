cf7b89ae2f88236f38be85705a70e98a00a9f9fe
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-15 19:56:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,62 +1,49 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
-public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    // Code here
-	    vector<int>color(V,-1);
-	    queue<int>q;
-	    
-	    for(int i = 0; i < V; i++){
-	        if(color[i] == -1){
-	            q.push(i);
-	            color[i] = 1;
-	            while(!q.empty()){
-	                int node = q.front();
-	                q.pop();
-	                
-	                for(int i = 0 ; i < adj[node].size(); i++){
-	                    if(color[adj[node][i]] == -1){
-	                        q.push(adj[node][i]);
-	                        
-	                        color[adj[node][i]] = (color[node] +1 )%2;
-	                    }
-	                    else{
-	                        if(color[adj[node][i]] == color[node])
-	                            return 0;
-	                    }
-	                }
-	            }
-	        }
-	    }
-	    
-	    return 1;
-	}
-
+  public:
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        // Code here
+        vector<vector<int>>adj(V);
+        
+        for(int i= 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int>color(V,-1);
+        queue<int>q;
+        
+        for(int i = 0; i < V; i++){
+            if(color[i] == -1){
+                 q.push(0);
+        color[0] = 0;
+        
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(int i = 0; i < adj[node].size(); i++){
+                //if color is not assign
+                if(color[adj[node][i]] == -1){
+                    color[adj[node][i]] = (color[node] + 1)%2;
+                    q.push(adj[node][i]);
+                }
+                
+                else{
+                    if(color[node] == color[adj[node][i]])return 0;
+                }
+            }
+        }
+        
+        
+        
+                
+            }
+        }
+        return 1;
+        
+    }
 };
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-		cin >> V >> E;
-		vector<int>adj[V];
-		for(int i = 0; i < E; i++){
-			int u, v;
-			cin >> u >> v;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-		Solution obj;
-		bool ans = obj.isBipartite(V, adj);    
-		if(ans)cout << "1\n";
-		else cout << "0\n";  
-	}
-	return 0;
-}
-// } Driver Code Ends
