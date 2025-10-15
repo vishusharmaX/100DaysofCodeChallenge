43282709cc5fe503ad359ea95bfa9c55e52f11a7
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-15 13:27:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,86 +1,47 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to return list containing vertices in Topological order. 
-	
-	void solve(int node,vector<int> adj[],vector<int>&visited,vector<int>&ans ){
-	    visited[node] = 1;
-	    
-	    for(int i = 0; i < adj[node].size(); i++){
-	        if(!visited[adj[node][i]])
-	            solve(adj[node][i],adj,visited,ans);
-	    }
-	    ans.push_back(node);
-	}
-	
-	
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    vector<int>visited(V,0);
-	    vector<int>ans;
-	    for(int i = 0; i < V; i++){
-	        if(!visited[i])
-	            solve(i,adj,visited,ans);
-	    }
-	    reverse(ans.begin(),ans.end());
-	    
-	    return ans;
-	    
-	}
-};
-
-//{ Driver Code Starts.
-
-/*  Function to check if elements returned by user
-*   contains the elements in topological sorted form
-*   V: number of vertices
-*   *res: array containing elements in topological sorted form
-*   adj[]: graph input
-*/
-int check(int V, vector <int> &res, vector<int> adj[]) {
-    
-    if(V!=res.size())
-    return 0;
-    
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++) {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++) {
-        for (int v : adj[i]) {
-            if (map[i] > map[v]) return 0;
+class Solution {
+  public:
+    void solve(int node, vector<vector<int>>& adj, vector<bool>&visited,vector<int>&ans ){
+        
+        visited[node]= 1;
+        
+        for(int i = 0 ; i < adj[node].size(); i++){
+            if(visited[adj[node][i]] == 0){
+                solve(adj[node][i],adj,visited, ans);
+            }
         }
+        // cout<<"node"<<node<<"->";
+        ans.push_back(node);
+        
     }
-    return 1;
-}
-
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
-
-        vector<int> adj[N];
-
-        for (int i = 0; i < E; i++) {
-            cin >> u >> v;
+  
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
             adj[u].push_back(v);
         }
-        
-        Solution obj;
-        vector <int> res = obj.topoSort(N, adj);
 
-        cout << check(N, res, adj) << endl;
+        vector<bool>visited(V,0);
+        vector<int>ans;
+        
+        for(int i = 0; i < V; i++){
+            if(visited[i] == 0){
+                solve(i, adj,visited, ans);
+            }
+        }   
+        
+        reverse(ans.begin(),ans.end());
+        // for(auto it: ans){
+        //     cout<<it<<" ";
+        // }
+        
+        // cout<<endl;
+        
+        return ans;
+        
+        
     }
-    
-    return 0;
-}
-// } Driver Code Ends
+};
