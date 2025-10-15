7dc24edf0595134916cc01ea3b885f831fc70c75
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-15 23:37:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Covid Spread/covid-spread.cpp
+++ Difficulty: Medium/Covid Spread/covid-spread.cpp
@@ -1,96 +1,62 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-
-#include<bits/stdc++.h> 
-using namespace std; 
-
-
-// } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 class Solution {
-public:
-    int helpaterp(vector<vector<int>> hospital)
-    {
-        //code here
-        int n = hospital.size();
-        int m = hospital[0].size();
+  public:
+    bool issafe(int x , int y , vector<vector<int>>&mat){
+        
+        return (x >= 0 && x < mat.size()  && y >= 0 && y < mat[0].size() && mat[x][y] == 1);
+        
+    }
+    
+    int helpaterp(vector<vector<int>> mat) {
+        // code here
+        int r = mat.size();
+        int c = mat[0].size();
         queue<pair<int,int>>q;
-        for(int i =  0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(hospital[i][j] == 2){
+        for(int i = 0; i < r;i++){
+            for(int j = 0; j< c; j++){
+                if(mat[i][j] == 2){
                     q.push({i,j});
                 }
             }
         }
         
-        int cnt = 0;
+        if(q.size() == 0)return 0;
+        
+        
+        int times = 0;
         while(!q.empty()){
-                cnt++;
-                int size = q.size();
-                while(size--){
-                     int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            int dirx[] = {-1,1,0,0};
-            int diry[] = {0,0,-1,1};
-            
-            for(int k = 0; k < 4; k++){
-                int newx = x + dirx[k];
-                int newy = y + diry[k];
+            times++;
+            int size = q.size();
+            while(size--){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                int dirx[] = {-1, 1, 0, 0};
+                int diry[] = {0, 0, -1, 1};
+    
                 
-                if(newx>=0 && newx < n && newy>=0 && newy < m && hospital[newx][newy]==1){
-                    hospital[newx][newy] = 2;
-                    q.push({newx,newy});
+                for(int i = 0; i < 4; i++){
+                    int newx = dirx[i]+row;
+                    int newy = diry[i] +col;
+                    if(issafe(newx,newy,mat)){
+                        q.push({newx,newy});
+                        mat[newx][newy] = 2;
+                    }
                 }
-            }
-                }
+                
             
+            }
         }
-        for(int i =  0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(hospital[i][j] == 1){
+       for(int i = 0; i < r;i++){
+            for(int j = 0; j< c; j++){
+                if(mat[i][j] == 1){
                     return -1;
                 }
             }
         }
         
+        return times-1;
         
-        return cnt-1;
     }
 };
-
-
-//{ Driver Code Starts.
-
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int R;int C;
-        
-        cin>>R>>C;
-        vector<vector<int>> hospital;
-        int i,j;
-        for(i=0;i<R;i++)
-         {   vector<int> temp;
-             for(j=0;j<C;j++)
-            {
-                int k;
-                cin>>k;
-                temp.push_back(k);
-            }
-            hospital.push_back(temp);
-         }
-        
-        Solution ob;
-        int ans = ob.helpaterp(hospital);
-        cout << ans << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
