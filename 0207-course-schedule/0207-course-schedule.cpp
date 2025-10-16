c1bf74ca6e877792193217675739e31e7db09685
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-17 02:03:00 +0530

    Time: 3 ms (79.34%), Space: 19.2 MB (85.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n  = prerequisites.size();
-        vector<int>adj[numCourses];
-        vector<int>indegree(numCourses,0);
+    bool canFinish(int N, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(N);
+        for(int i = 0; i < prerequisites.size(); i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            
+            adj[v].push_back(u);
+        }
+        
+        
+        vector<int>indeg(N,0);
+        
+       for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj[i].size(); j++){
+                indeg[adj[i][j]]++;
+            }   
 
-        for(int i = 0 ; i < n ; i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
         }
         queue<int>q;
-        for(int i = 0 ; i<numCourses ; i++){
-            if(indegree[i] == 0)
+        for(int i = 0; i < indeg.size(); i++){
+            if(indeg[i] == 0){
                 q.push(i);
+            }
         }
+        
         int cnt = 0;
         while(!q.empty()){
             int node = q.front();
             q.pop();
             cnt++;
-
-            for(int i = 0; i <adj[node].size(); i++){
-                indegree[adj[node][i]]--;
-                if(indegree[adj[node][i]] == 0)
+            for(int i = 0; i < adj[node].size(); i++){
+                indeg[adj[node][i]]--;
+                if(indeg[adj[node][i]] == 0){
                     q.push(adj[node][i]);
+                }
             }
         }
-
-
-        return cnt == numCourses;
-
+        
+        
+        return cnt == N;
     }
 };
