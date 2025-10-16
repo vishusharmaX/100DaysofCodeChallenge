642ebfc7a89e96bc8d4b1988a85c9dadd1fd4950
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-17 02:01:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prerequisite Tasks/prerequisite-tasks.cpp
+++ Difficulty: Medium/Prerequisite Tasks/prerequisite-tasks.cpp
@@ -1,67 +1,46 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
-public:
-	bool isPossible(int N,int P, vector<pair<int, int> >& prerequisites) {
-	    // Code here
-	   //use the kahn's Algorithm -> cycle detection and DAG
-	   
-	   vector<int>adj[N];
-	   vector<int>indegree(N,0);
-	   
-	   for(int i = 0; i < P ; i++){
-	       adj[prerequisites[i].second].push_back(prerequisites[i].first);
-	       indegree[prerequisites[i].first]++;
-	   }
-	   
-	   queue<int>q;
-	   for(int i = 0 ; i < N; i++){
-	       if(indegree[i] == 0)
-	        q.push(i);
-	   }
-	   int cnt = 0;
-	   while(!q.empty()){
-	       int node= q.front();
-	       q.pop();
-	       cnt++;
-	       for(int i = 0 ; i < adj[node].size(); i++){
-	           indegree[adj[node][i]]--;
-	           if(indegree[adj[node][i]] == 0)
-	            q.push(adj[node][i]);
-	       }
-	   }
-	   
-	   return cnt == N;
-	    
-	}
-};
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-    	int N, P;
-        vector<pair<int, int> > prerequisites;
-        cin >> N;
-        cin >> P;
-        for (int i = 0; i < P; ++i) {
-            int x, y;
-            cin >> x >> y;
-            prerequisites.push_back(make_pair(x, y));
+  public:
+    bool isPossible(int N, int P, vector<pair<int, int> >& prerequisites) {
+        // Code here
+        vector<vector<int>>adj(N);
+        
+        for(int i = 0; i < P; i++){
+            int u = prerequisites[i].first;
+            int v = prerequisites[i].second;
+            
+            adj[v].push_back(u);
         }
-        // string s;
-        // cin>>s;
-        Solution ob;
-        if (ob.isPossible(N,P, prerequisites))
-            cout << "Yes";
-        else
-            cout << "No";
-        cout << endl;
-	}
-	return 0;
-}
-// } Driver Code Ends
+        
+        
+        vector<int>indeg(N,0);
+        
+       for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj[i].size(); j++){
+                indeg[adj[i][j]]++;
+            }   
+
+        }
+        queue<int>q;
+        for(int i = 0; i < indeg.size(); i++){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        int cnt = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for(int i = 0; i < adj[node].size(); i++){
+                indeg[adj[node][i]]--;
+                if(indeg[adj[node][i]] == 0){
+                    q.push(adj[node][i]);
+                }
+            }
+        }
+        
+        
+        return cnt == N;
+    }
+};
