4d716d25a94400389e3cb1ff2b6273f71e22ce05
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-17 01:49:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -19,23 +19,44 @@ class Solution {
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>>adj(V);
-        vector<int>path(V,0);
-         vector<int>visited(V,0);
         for(int i = 0; i < edges.size();i++){
             int u = edges[i][0];
             int v = edges[i][1];
             
             adj[u].push_back(v);
         }
+        vector<int>indeg(V,0);
         
-        for(int i = 0; i < V;i++){
-            if(visited[i] == 0 && solve(i,adj,visited,path)){
-                return 1;
+        for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj[i].size(); j++){
+                indeg[adj[i][j]]++;
             }
         }
         
-        return 0;
+        queue<int>q;
         
+        for(int i = 0; i <indeg.size(); i++){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        int cnt = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            
+            for(int i = 0; i < adj[node].size(); i++){
+                indeg[adj[node][i]]--;
+                if(indeg[adj[node][i]] ==0){
+                    q.push(adj[node][i]);
+                }
+            }
+        }
+        
+        
+        return cnt != V;
         
         
     }
