93420086a296aeb5151e4ea740b6e473012c9d06
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-17 02:39:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -1,101 +1,64 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution{
-    public:
-    string ans;
-    vector<bool> vis,cur;
-    unordered_set<int> g[26]; // Graph
-    
-    void topological_sort(int r) {
-        vis[r]=cur[r]=1;
-        for(int i:g[r]) 
-            if(!vis[i])
-                topological_sort(i);
-        
-        ans=char(97+r)+ans;
-        cur[r]=0;
+class Solution {
+public:
+    void solve(string s, string t, vector<vector<int>>& adj, vector<int>& indegree) {
+        int len = min(s.length(), t.length());
+        for (int i = 0; i < len; i++) {
+            if (s[i] != t[i]) {
+                int u = s[i] - 'a';
+                int v = t[i] - 'a';
+                adj[u].push_back(v);
+                indegree[v]++;
+                break;
+            }
+        }
     }
-    
-    string findOrder(string arr[], int N, int K) {
-        ans="";
-        vis.assign(26,0); 
-        cur=vis;
-      
-         for(int i=1;i<N;i++) {
-          string a=arr[i-1],b=arr[i];
-          int l=0,n=min(a.size(),b.size());
-          while(l<n && a[l]==b[l]) 
-                l++;
-          if(l!=n) g[a[l]-'a'].insert(b[l]-'a');
-         }
-      
-       for(int i=0;i<K;i++)
-            if(!vis[i])
-                topological_sort(i);
-        return ans;
+
+    string findOrder(vector<string> &words) {
+        vector<vector<int>> adj(26);
+        vector<int> indegree(26, 0);
+        vector<bool> present(26, false);
+
+        // Mark all present characters
+        for (auto &word : words)
+            for (char c : word)
+                present[c - 'a'] = true;
+
+        // Build graph and validate prefix rule
+        for (int i = 0; i < words.size() - 1; i++) {
+            string s1 = words[i];
+            string s2 = words[i + 1];
+
+            // Prefix invalidation check
+            if (s1.length() > s2.length() && s1.substr(0, s2.length()) == s2) {
+                return "";  // Invalid case: "abc" before "ab"
+            }
+
+            solve(s1, s2, adj, indegree);
+        }
+
+        // Topological sort (Kahn's Algorithm)
+        queue<int> q;
+        for (int i = 0; i < 26; i++) {
+            if (indegree[i] == 0 && present[i])
+                q.push(i);
+        }
+
+        string result;
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            result += (u + 'a');
+            for (int v : adj[u]) {
+                indegree[v]--;
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+
+        // If result length != number of present characters, there's a cycle
+        int totalChars = count(present.begin(), present.end(), true);
+        if (result.length() != totalChars)
+            return "";  // Cycle detected
+
+        return result;
     }
 };
-
-
-
-//{ Driver Code Starts.
-string order;
-
-bool f(string a, string b) {
-    int p1 = 0;
-    int p2 = 0;
-    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
-        p1 = order.find(a[i]);
-        p2 = order.find(b[i]);
-        //	cout<<p1<<" "<<p2<<endl;
-    }
-
-    if (p1 == p2 and a.size() != b.size())
-        return a.size() < b.size();
-
-    return p1 < p2;
-}
-
-// Driver program to test above functions
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, K;
-        cin >> N >> K;
-        string dict[N];
-        for (int i = 0; i < N; i++)
-            cin >> dict[i];
-
-        Solution obj;
-        string ans = obj.findOrder(dict, N, K);
-        order = "";
-        for (int i = 0; i < ans.size(); i++)
-            order += ans[i];
-
-        string temp[N];
-        std::copy(dict, dict + N, temp);
-        sort(temp, temp + N, f);
-
-        bool f = true;
-        for (int i = 0; i < N; i++)
-            if (dict[i] != temp[i])
-                f = false;
-
-        if (f)
-            cout << 1;
-        else
-            cout << 0;
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
