a5d8ab08e05f6f05c5ed8dde20756f9a19801e7b
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-18 22:57:35 +0530

    Time: 53 ms (63.57%), Space: 136.1 MB (69.28%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -1,39 +1,46 @@
-class Solution {
-public:
-    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-
-        vector<int>adj[n];
-        vector<int>Indeg(n,0);
-
-        for(int i =0;i<relations.size(); i++){
-            adj[relations[i][0]-1].push_back(relations[i][1]-1);
-            Indeg[relations[i][1]-1]++;
-        }
-
-        queue<int>q;
-        for(int i = 0; i < n; i++)
-        if(!Indeg[i])
-        q.push(i);
-        vector<int>coursetime(n,0);
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            for(int i =0;  i < adj[node].size(); i++){
-                Indeg[adj[node][i]]--;
-                if(!Indeg[adj[node][i]])
-                q.push(adj[node][i]);
-                coursetime[adj[node][i]] = max(coursetime[adj[node][i]],coursetime[node]+time[node]);
+class Solution
+{
+    public:
+        int minimumTime(int n, vector<vector < int>> &relations, vector< int > &time)
+        {
+            vector<vector<int>>adj(n);
+            for(int i = 0; i < relations.size(); i++){
+                adj[relations[i][0]-1].push_back(relations[i][1]-1);
             }
+
+            vector<int>indeg(n,0);
+            for(int i= 0; i < adj.size(); i++){
+                for(int j =0; j < adj[i].size(); j++){
+                    indeg[adj[i][j]]++;
+                }
+            }
+            queue<int>q;
+            for(int i = 0; i < indeg.size(); i++){
+                if(indeg[i] == 0){
+                    q.push(i);
+                }
+            }
+            vector<int>coursetime(n,0);
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+
+                for(int i= 0; i < adj[node].size();i++){
+                    indeg[adj[node][i]]--;
+                    if(indeg[adj[node][i]] == 0){
+                        q.push(adj[node][i]);
+                    }
+                    coursetime[adj[node][i]] = max(coursetime[adj[node][i]] , coursetime[node]+ time[node]);
+                }
+            }
+
+            int ans = 0;
+            for(int i = 0; i < n;i++){
+                ans = max(ans,coursetime[i] + time[i]);
+            }
+
+
+            return ans;
+
         }
-
-        int ans = 0;
-
-        for(int i = 0; i < n;i++){
-            ans = max(ans,coursetime[i]+time[i]);
-        }
-
-        return ans;
-        
-    }
 };
