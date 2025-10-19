4ec4f8efefa6ce4842045222a5574c804cfbfb2d
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-19 23:44:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,88 +1,54 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
-    void DFS(int node,vector<pair<int,int>> adj[] ,vector<bool>&visited,stack<int>&st){
+    void solve(int node,vector<vector<pair<int,int>>>adj, vector<int>&visited,stack<int>&st){
         visited[node] = 1;
         
         for(int i = 0;i <adj[node].size(); i++){
-            if(!visited[adj[node][i].first])
-                DFS(adj[node][i].first,adj,visited,st);
+            if(visited[adj[node][i].first] == 0){
+                solve(adj[node][i].first,adj,visited,st);
+            }
         }
         
         st.push(node);
     }
-     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
         // code here
-        vector<pair<int,int>>adj[N];
+        vector<vector<pair<int,int>>> adj(V);  // Resize to V
+
         
-        //phele adjacency matrix banao
-        /// toplogical sort laago DFS
-        for(int i =0 ; i < M ; i++){
+        for(int i= 0; i < edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             int weight = edges[i][2];
-            
-            adj[u].push_back(make_pair(v,weight));
+            adj[u].push_back({v,weight});
         }
         
         stack<int>st;
-        vector<bool>visited(N,0);
-        DFS(0,adj,visited,st);
+        vector<int>visited(V,0);
+        solve(0,adj,visited,st);
         
-        vector<int>ans(N,INT_MAX);
-        ans[0] = 0;
+       
+        vector<int>dist(V,INT_MAX);
+         dist[0] = 0;
         while(!st.empty()){
             int node = st.top();
             st.pop();
             
             for(int i = 0; i < adj[node].size(); i++){
-                int neighbour = adj[node][i].first;
+                int neig = adj[node][i].first;
                 int weight = adj[node][i].second;
-                ans[neighbour] = min(ans[neighbour],weight + ans[node]);
+                
+                dist[neig] = min(dist[neig] , dist[node] + weight);
             }
         }
         
-        for(int i = 0; i < N; i++){
-            if(ans[i] == INT_MAX)
-                ans[i] = -1;
+        for(int i = 0; i < dist.size(); i++){
+            if(dist[i] == INT_MAX)
+                dist[i] = -1;
         }
         
-        return ans;
+        return dist;
         
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> edges;
-        for(int i=0; i<m; ++i){
-            vector<int> temp;
-            for(int j=0; j<3; ++j){
-                int x; cin>>x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-        Solution obj;
-        vector<int> res = obj.shortestPath(n, m, edges);
-        for (auto x : res) {
-            cout << x << " ";
-        }
-        cout << "\n";
-    }
-}
-
-// } Driver Code Ends
