77feb24deb3f1af141b81fd039387c42a8b2dfa6
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2025-10-21 02:45:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,90 +1,42 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User Function Template
 class Solution {
   public:
-    // Function to find the shortest distance of all the vertices
-    // from the source vertex src.
-    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
-        
-        int V = adj.size();
-        
-        vector<bool>explored(V,0);
-        vector<int>dist(V,INT_MAX);
-        
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Step 1: Build adjacency list
+        vector<pair<int, int>> adj[V]; 
+        for (auto &edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w}); 
+        }
+
+      
+        vector<int> explored(V, 0);
+        vector<int> dist(V, INT_MAX);
         dist[src] = 0;
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,src});
         
-        int cnt = V;
-        
-        while(cnt--){
+        while (!pq.empty()) {
+            int node = pq.top().second;
+            pq.pop();
+
+            if(explored[node] == 1)continue;
             
-        
-        int node = -1 , value = INT_MAX;
-        for(int i = 0; i < V; i++){
-            if(!explored[i] &&  value > dist[i] ){
-                node = i;
-                value = dist[i];
+            explored[node] = 1;
+            
+            for (int j = 0; j < adj[node].size(); j++) {
+                int neig = adj[node][j].first;
+                int weight = adj[node][j].second;
+
+                if (!explored[neig] && dist[node] + weight < dist[neig]) {
+                    dist[neig] = dist[node] + weight;
+                    pq.push({dist[neig],neig});
+                }
             }
         }
-        
-        explored[node] = 1;
-        
-        
-        for(int i = 0; i < adj[node].size(); i++){
-            int neighbour = adj[node][i].first;
-            int weight = adj[node][i].second;
-            
-            if(!explored[neighbour]  && (dist[node] + weight < dist[neighbour])){
-                dist[neighbour] = dist[node] + weight;
-            }
-            
-        }
-        
-        
-        }
-        
-        
+
         return dist;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<pair<int, int>>> adj(V);
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            pair<int, int> t1 = {v, w}, t2 = {u, w};
-            adj[u].push_back(t1);
-            adj[v].push_back(t2);
-        }
-        int src;
-        cin >> src;
-        cin.ignore();
-
-        Solution obj;
-        vector<int> res = obj.dijkstra(adj, src);
-
-        for (int i = 0; i < V; i++)
-            cout << res[i] << " ";
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-// } Driver Code Ends
