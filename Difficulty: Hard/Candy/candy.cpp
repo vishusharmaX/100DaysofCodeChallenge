b6cf2e4e3eaaced8143e45bf084e27564c238ae5
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-01-15 19:22:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Candy/candy.cpp
+++ Difficulty: Hard/Candy/candy.cpp
@@ -1,60 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  public:
-    int minCandy(int n, vector<int> &arr) {
-        // code here
-        vector<int>left(n,1);
-        vector<int>right(n,1);
-        
-        for(int i =1;i < n; i++){
-            if(arr[i] > arr[i-1]){
-                left[i] = left[i-1]+1;
+public:
+    int minCandy(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> candies(n, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
-            else
-                left[i] = 1;
         }
-        
-        for(int i = n-2; i>=0; i--){
-            if(arr[i] > arr[i+1])
-                right[i] = right[i+1]+1;
-            else    
-                right[i] = 1;
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
         }
-        
-        
-        int sum = 0;
-        
-        for(int i = 0; i < n; i++){
-            sum = sum + max(left[i],right[i]);
-        }
-        
-        return sum;
-        
+
+
+        int total = 0;
+        for (int c : candies) total += c;
+
+        return total;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-
-        vector<int> ratings(N);
-        for (int i = 0; i < N; i++) {
-            cin >> ratings[i];
-        }
-        Solution obj;
-        cout << obj.minCandy(N, ratings) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
