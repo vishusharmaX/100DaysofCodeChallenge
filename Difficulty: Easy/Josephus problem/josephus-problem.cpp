1db7e984ad0d30e84f23f593c82d22f17124f174
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-01-24 17:21:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Josephus problem/josephus-problem.cpp
+++ Difficulty: Easy/Josephus problem/josephus-problem.cpp
@@ -1,51 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-
-// } Driver Code Ends
-/*You are required to complete this method */
-
-class Solution
-{
-    public:
-    int josephus(int n, int k)
-    {
-       //Your code here
-       vector<int>ans(n);
-       for(int i = 0; i < n; i++){
-           ans[i] = i +1;
-       }
-       
-       int i = 0;
-       while(ans.size() > 1){
-           int indx = (i+k-1)%ans.size();
-           ans.erase(ans.begin()+ indx);
-           i = indx;
-       }
-       
-       return ans[0];
+class Solution {
+  public:
+    int solve(int n ,int k){
+        if(n == 1){
+            return 0;
+        }
+        
+        return (solve(n-1,k)+k)%n;
+    }
+  
+    int josephus(int n, int k) {
+        // code here
+        return solve(n,k) + 1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-	
-	int t;
-	cin>>t;//testcases
-	while(t--)
-	{
-		int n,k;
-		cin>>n>>k;//taking input n and k
-		
-		//calling josephus() function
-		Solution ob;
-		cout<<ob.josephus(n,k)<<endl;
-	}
-	return 0;
-}
-// } Driver Code Ends
