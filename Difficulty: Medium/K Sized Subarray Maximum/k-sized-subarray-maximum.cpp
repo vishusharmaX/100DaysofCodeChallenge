208fa9efa7fe0bf4707b303c09245d586ab37954
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-02-01 09:38:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -5,20 +5,19 @@ class Solution {
         int n = arr.size();
         deque<int>dq;
         vector<int>ans;
-        for(int i  = 0; i < k; i++){
-            // cout<<arr[i]<<" ";
-            // cout<<endl;
+        for(int i = 0; i < k ;i++){
+            
             while(!dq.empty() && arr[dq.back()] < arr[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
         }
         
-        // cout<<"f ele"<<dq.front();
         ans.push_back(arr[dq.front()]);
         
         
-        for(int i=k; i< n; i++){
+        for(int i = k ; i < n ; i++){
+            
             while(!dq.empty() && i - dq.front() >= k){
                 dq.pop_front();
             }
@@ -27,14 +26,11 @@ class Solution {
             while(!dq.empty() && arr[dq.back()] < arr[i]){
                 dq.pop_back();
             }
-            
             dq.push_back(i);
             ans.push_back(arr[dq.front()]);
-            
-            
         }
         
-        return ans;
         
+        return ans;
     }
 };
