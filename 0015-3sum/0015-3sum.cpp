b62e167d0222e815cbb22d3de90173d16b2c4bb4
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-04-20 03:36:10 +0530

    Time: 151 ms (11.31%), Space: 35 MB (12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,33 +4,43 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int j = i + 1;
-            int k = n - 1;
+        for(int i = 0;  i < n-2; i++){
+            if(i > 0 && nums[i-1] == nums[i])continue;
+            int left = i+1;
+            int right = n-1;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int sum = -1 * nums[i];
 
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
+            while(left < right){
+                vector<int>temp;
+                if(nums[left] + nums[right] == sum){
+                    temp.push_back(nums[i]);
+                     temp.push_back(nums[left]);
+                      temp.push_back(nums[right]);
+                    left++;
+                    right--;
+                    
+
+                    while(left < right  &&  nums[left-1] == nums[left] ){
+                        left++;
+                    }
+                    while(left < right  &&  nums[right] == nums[right+1] ){
+                        right--;
+                    }
+
                     ans.push_back(temp);
-                    j++;
-                    k--;
-
-                    while (j<k && nums[j] == nums[j - 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1])
-                        k--;
                 }
+                else if(nums[left] + nums[right] < sum){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+
             }
         }
 
         return ans;
+        
     }
 };
