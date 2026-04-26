ca7d5ee545a5ac7fd486ae4c6d997e032262bd3e
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-04-26 18:34:00 +0530

    Time: 62 ms (10.29%), Space: 84.6 MB (5.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int minDistance(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        
-        for(int i = 0; i < n+1; i++){
-            dp[i][0] = i;
+    int f(int i , int j, string s, string t,vector<vector<int>>&dp){
+
+        if(i < 0)  return j+1;
+        if(j < 0) return i+1;
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        for(int j = 0; j < m+1; j++){
-            dp[0][j] = j;
-        }
-        
-        
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < m+1; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] =  dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-                }
-            }
-        }
-        
-        return dp[n][m];
-        
+
+        if(s[i] == t[j])return dp[i][j] = f(i-1,j-1,s,t,dp);
+
+        return  dp[i][j] = 1 + min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n-1,m-1,word1,word2,dp);
     }
 };
