c92b3594a5b10bce428dedf2fef11c1d3a1a69f7
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-02 12:42:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
@@ -9,20 +9,17 @@ class Solution {
         for(int i = 0;i < n+1; i++)dp[i][0] = 0;
         
         
-        
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < sum+1; j++){
                 if(coins[i-1] <= j){
-                    dp[i][j] = min(1+ dp[i][j-coins[i-1]] , dp[i-1][j]);
-                }else{
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]] , dp[i-1][j]);
+                }
+                else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
         
         return dp[n][sum] == INF ? -1 : dp[n][sum];
-        
-        
-        
     }
 };
