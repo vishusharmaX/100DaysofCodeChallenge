925a16cd0cf74e9084ffba6ed724ac29994fa904
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-03 10:09:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+  
+    void solve(int node,vector<vector<int>>&adj ,vector<int>&visited,vector<int>&ans){
+        
+        visited[node] = 1;
+        
+        for(int i = 0; i < adj[node].size(); i++){
+            if(!visited[adj[node][i]]){
+                solve(adj[node][i],adj, visited, ans);
+            }
+        }
+        
+        
+        ans.push_back(node);
+    }
+    
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        
+        vector<vector<int>>adj(V);
+        
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<int>visited(V,0);
+        vector<int>ans;
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                solve(i,adj,visited,ans);
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
