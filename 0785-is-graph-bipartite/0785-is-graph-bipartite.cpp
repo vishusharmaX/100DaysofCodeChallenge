847cd3b3017b024ddbd4c985b5dd07f7b43466d7
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-03 12:51:13 +0530

    Time: 0 ms (100%), Space: 17.5 MB (60.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,42 +2,40 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& adj) {
         
+
         int V = adj.size();
         vector<int>color(V,-1);
         queue<int>q;
-
-
         
         for(int i = 0; i < V; i++){
+            
             if(color[i] == -1){
-                q.push(i);
-        color[i] = 1;
+            q.push(i);
+            color[i] = 0;
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
 
-            for(int i = 0; i < adj[node].size(); i++){
-                //assign color to node
-
-                if(color[adj[node][i]] == -1){
-                    color[adj[node][i]] = (color[node] + 1)%2;
-                    q.push(adj[node][i]);
-                }
-
-                else{
-                    if(color[node] == color[adj[node][i]]){
-                        return 0;
+                for(int i = 0; i < adj[node].size(); i++){
+                    if(color[adj[node][i]] == -1){
+                        color[adj[node][i]] = (color[node] + 1)%2;
+                        q.push(adj[node][i]);
+                    }
+                    else if(color[node] == color[adj[node][i]]){
+                        return false;
                     }
                 }
+            }
 
             }
-        }
-            }
+
         }
 
+        return true;
 
-        return 1;
+        
+
 
     }
 };
