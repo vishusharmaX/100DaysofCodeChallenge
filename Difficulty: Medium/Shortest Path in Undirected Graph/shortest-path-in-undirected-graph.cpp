a266de363a3e56c02ba45e629aaaecb20aeb5f37
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-03 19:15:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
@@ -1,34 +1,38 @@
 class Solution {
   public:
-    // Function to find the shortest path from source to all other nodes
-    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+    vector<int> shortestPath(int V, vector<vector<int>> &edges, int src) {
         // code here
-        int n = adj.size();
-        vector<int>dist(n,-1);
-        vector<int>visited(n,0);
+        vector<vector<int>>adj(V);
+        
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int>dist(V,-1);
+        vector<int>visited(V,0);
         
         queue<int>q;
         q.push(src);
-        visited[src] = 1;
         dist[src] = 0;
+        visited[src] = 1;
         
         while(!q.empty()){
             int node = q.front();
             q.pop();
             
-            for(int i= 0; i < adj[node].size(); i++){
-                if(visited[adj[node][i]])continue;
-                
-                visited[adj[node][i]]=1;
-                q.push(adj[node][i]);
-                dist[adj[node][i]] = dist[node]+1;
+            for(int i = 0; i < adj[node].size(); i++){
+                if(!visited[adj[node][i]]){
+                    visited[adj[node][i]] = 1;
+                    dist[adj[node][i]] =  dist[node]+1;
+                    q.push(adj[node][i]);
+                }
             }
         }
         
         return dist;
-        
-        
-        
-        
     }
 };
