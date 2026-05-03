8ccb2b0a83e0524c161f4565783972818fc3046c
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-03 18:42:16 +0530

    Time: 24 ms (78.43%), Space: 18.3 MB (38.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    bool issafe(int x,int y, vector<vector<char>>&grid){
-        
-        return (x>=0 && x< grid.size() && y >=0 && y < grid[0].size()  && grid[x][y] =='1');
-    }
-    
     int numIslands(vector<vector<char>>& grid) {
-         int r = grid.size();
-        int c = grid[0].size();
         
+
         queue<pair<int,int>>q;
-        
         int cnt = 0;
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
+        for(int i = 0; i < grid.size(); i++){
+            for(int j= 0; j < grid[0].size(); j++){
                 if(grid[i][j] == '1'){
-                    cnt++;
                     q.push({i,j});
                     grid[i][j] = '0';
+                    cnt++;
                     while(!q.empty()){
-                        int x = q.front().first;
-                        int y = q.front().second;
+                        pair<int,int> node = q.front();
+                        int x = node.first;
+                        int y = node.second;
                         q.pop();
-                        
-                        int dirx[] = {-1,1,0,0};
-                        int diry[] = {0,0,-1,1};
+                        int dirx[] = {0,0,-1,1};
+                        int diry[] = {-1,1,0,0};
 
-                        
-                        for(int k = 0;k < 4; k++){
+                        for(int k = 0; k < 4; k++){
                             int newx = x + dirx[k];
                             int newy = y + diry[k];
-                            
-                            if(issafe(newx,newy,grid)){
+
+                            if(newx >= 0 && newx < grid.size()  && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == '1'){
                                 q.push({newx,newy});
                                 grid[newx][newy] = '0';
-                                
                             }
                         }
                     }
                 }
             }
         }
-        
+
         return cnt;
+
+
     }
 };
