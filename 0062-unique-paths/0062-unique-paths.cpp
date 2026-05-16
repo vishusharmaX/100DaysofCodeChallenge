a6821556a7107c51de01e2340c45c32e8dd76d90
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-16 11:40:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-    int solve(int n , int m){
-        if(n == 1 || m== 1){
+    bool ispos(int i , int j , int m , int n){
+
+        return i >= 0 && i < m  && j >= 0 && j < n;
+    }
+    int solve(int i, int j , int m, int n,vector<vector<int>>&dp){
+        if(i == m-1 && j == n-1){
             return 1;
+        }   
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        return solve(n,m-1) + solve(n-1,m);
-    }
+        int right = 0 , down = 0;
+        if(ispos(i,j+1,m,n)){
+            right =  solve(i,j+1,m,n,dp);
+        }
+        if(ispos(i+1,j,m,n)){
+            down =  solve(i+1,j,m,n,dp);
+        }
 
+        return dp[i][j] = right + down;
+    }
     int uniquePaths(int m, int n) {
-        
-        return solve(n,m);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(0,0,m,n,dp);
+
     }
 };
