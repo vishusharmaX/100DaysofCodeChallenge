0fc90f6f871c5a369e06f442cc77ba779d3f973c
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-16 22:32:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -1,62 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> subarraySum(vector<int> &arr, int target) {
         // code here
-        int n = arr.size();
-        int sum = 0 , i = 0, j = 0;
         
-        while(j < n){
-            sum += arr[j];
+        int i = 0 , j = 0;
+        int currsum = 0;
+        int n = arr.size();
+        while( j < n){
+            currsum += arr[j];
             
-            while(sum > target && i < j ){
-                sum-=arr[i];
-                i++;
+            if(currsum > target){
+                while(currsum > target){
+                    currsum -= arr[i];
+                    i++;
+                }
             }
-            if(sum == target)return {i+1,j+1};
+            
+            if(currsum == target){
+                return {i+1,j+1};
+            }
             j++;
         }
+        
         
         return {-1};
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        int d;
-        string input;
-
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        cin >> d;
-        cin.ignore();
-
-        Solution ob;
-        vector<int> result = ob.subarraySum(arr, d);
-        for (int i : result) {
-            cout << i << " ";
-        }
-        cout << "\n~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
