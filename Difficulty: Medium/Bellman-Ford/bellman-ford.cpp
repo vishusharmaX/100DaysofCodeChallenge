bda965ba8e5600bf0e8391059c4a7b30587b01ec
Vishwajeet Sharma <91490950+vishusharmaX@users.noreply.github.com>
2026-05-23 11:49:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -6,31 +6,38 @@ class Solution {
         // Code here
         vector<int>dist(V,1e8);
         dist[src] = 0;
+        
+        
         for(int i = 0; i < V-1; i++){
             
-            for(int j = 0; j < edges.size(); j++){
-                int u = edges[j][0];
-                int v = edges[j][1];
-                int weight = edges[j][2];
-                
+            for(int i = 0; i < edges.size(); i++){
+                int u = edges[i][0];
+                int v = edges[i][1];
+                int wt = edges[i][2];
                 if(dist[u] == 1e8)continue;
-                if(dist[u] + weight < dist[v]){
-                    dist[v] = dist[u] + weight;
+                if(dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
                 }
             }
         }
-            for(int j = 0; j < edges.size(); j++){
-                int u = edges[j][0];
-                int v = edges[j][1];
-                int weight = edges[j][2];
-                 if(dist[u] == 1e8)continue;
-                if(dist[u] + weight < dist[v]){
-                   vector<int>ans;
-                   ans.push_back(-1);
-                   return ans;
+        
+        
+        
+        for(int i = 0; i < edges.size(); i++){
+             for(int i = 0; i < edges.size(); i++){
+                int u = edges[i][0];
+                int v = edges[i][1];
+                int wt = edges[i][2];
+                if(dist[u] == 1e8)continue;
+                if(dist[u] + wt < dist[v]){
+                    vector<int>ans;
+                    ans.push_back(-1);
+                    return ans;
                 }
             }
+        }
         
         return dist;
+       
     }
 };
